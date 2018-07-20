0a0d82f9633ae1668e18335ae789c0f251c65ac1
Yaron Weiss <YARON.WEISS@amdocs.com>
2018-07-19 22:52:08 -0500

    more console.writeline for socket server

--- Ginger/GingerCoreNET/Drivers/CommunicationProtocol/SocketHelper.cs
+++ Ginger/GingerCoreNET/Drivers/CommunicationProtocol/SocketHelper.cs
@@ -48,7 +48,7 @@ namespace Amdocs.Ginger.CoreNET.Drivers.CommunicationProtocol
 
         //Verify we provide one port at a time, for unit test this function can be called in parallel
         static Semaphore semaphore = new Semaphore(3, 10);  // Enable 3 port allocator at a time with max up to 10        
-        static int LastPort = 15000;  // Ginger Grid and nodes will be on 15000+ ports - this area is mainly free to find ports
+        static int LastPort = 50000;  // Ginger Grid and nodes will be on 15000+ ports - this area is mainly free to find ports
         public static int GetOpenPort()
         {
             semaphore.WaitOne(); // control the reentry if several threads request at the same time                        
