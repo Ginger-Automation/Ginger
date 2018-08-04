a66b36d31aacfa6bc09e22e6baece4f4fe8084a3
Mohd Ambar Khan <MohdAmbar.Khan@amdocs.com>
2018-08-04 14:20:10 +0530

    Adding GingerWPFDriver and Ginger WPF Driver Test

---
+++ Ginger/GingerWPFDriverWindowTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GingerWPFDriverWindowTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GingerWPFDriverWindowTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11d28bcf-ea5c-472a-a8fe-28df1598a9e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
