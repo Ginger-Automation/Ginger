cd92d0bdef33aa471a8de2da8a2940b09a909edd
Haim Uzan <uzanhaim@gmail.com>
2018-08-23 11:36:36 +0300

    test

--- Ginger/GingerCore/ALM/RQM/ImportFromRQM.cs
+++ Ginger/GingerCore/ALM/RQM/ImportFromRQM.cs
@@ -84,7 +84,7 @@ namespace GingerCore.ALM.RQM
                         repoActivsGroup = GingerActivitiesGroupsRepo.Where(x => x.ExternalID != null ? x.ExternalID.Split('|').First().Split('=').Last() == tc.RQMID : false).FirstOrDefault();
                     if (repoActivsGroup != null)
                     {
-                        tcActivsGroup = (ActivitiesGroup)repoActivsGroup.CreateInstance(true);
+                        tcActivsGroup = (ActivitiesGroup)repoActivsGroup.CreateInstance(true); //test
                         tcActivsGroup.ExternalID = tcActivsGroup.ExternalID.Replace("RQMRecordID=" + ExportToRQM.GetExportedIDString(tcActivsGroup.ExternalID, "RQMRecordID"), "RQMRecordID=");
                         busFlow.AddActivitiesGroup(tcActivsGroup);
                         busFlow.ImportActivitiesGroupActivitiesFromRepository(tcActivsGroup, GingerActivitiesRepo, true, true);
