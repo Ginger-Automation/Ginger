3f7a0285df3a7e4fb34962241f4b8e95bb028b21
Yaron Weiss <YARON.WEISS@amdocs.com>
2018-09-26 16:50:49 -0500

    ActPluginEditPage - dynamic page per param with VE

--- Ginger/Ginger/Actions/PlugIns/ActPlugInEditPage.xaml.cs
+++ Ginger/Ginger/Actions/PlugIns/ActPlugInEditPage.xaml.cs
@@ -18,6 +18,7 @@ limitations under the License.
 
 using amdocs.ginger.GingerCoreNET;
 using Amdocs.Ginger.Repository;
+using Ginger.UserControlsLib.ActionInputValueUserControlLib;
 using GingerCore;
 using GingerCore.Actions.PlugIns;
 using GingerPlugIns.ActionsLib;
@@ -42,6 +43,7 @@ namespace Ginger.Actions.PlugIns
             InitializeComponent();
             mAct = act;
             LoadEditPage();
+            AutoCreateEditPage();
         }
 
         //TODO: destructor is not working , need to fix geneal ActionEditPage
@@ -123,7 +125,7 @@ namespace Ginger.Actions.PlugIns
                 mAct.AddOrUpdateInputParamValue(((ActionParam)sender).Name, ((ActionParam)sender).Value.ToString());
         }
 
-        // We Bind the controls on the page to the action input params based ont he Xaml object Name - x:Name=
+        // We Bind the controls on the page to the action input params based on the Xaml object Name - x:Name=
         //this is recursive function which drill down when needed
         private void BindControlsToAction(Panel container)
         {         
@@ -143,6 +145,39 @@ namespace Ginger.Actions.PlugIns
 
                     //TODO: check control type and bind per type
                 }
+            }
+        }
+
+
+        private void AutoCreateEditPage()
+        {
+            int rows = mAct.InputValues.Count;
+            for (int i = 0; i < rows; i++)
+            {
+                ActionConfigGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(35) });
+            }
+
+            ActionConfigGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(30, GridUnitType.Star) });
+            ActionConfigGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(70, GridUnitType.Star) });
+
+            int rnum = 0;
+            foreach (ActInputValue param in mAct.InputValues)
+            {
+                Label l = new Label() { Content = param.Param };
+                ActionConfigGrid.Children.Add(l);
+                l.Style = App.GetStyle("@InputFieldLabelStyle");
+                Grid.SetRow(l, rnum);
+
+
+                //TODO: based on the param type create textbox, check box, combo, etc...
+
+                ActionInputValueUserControl actionInputValueUserControl = new ActionInputValueUserControl();
+                actionInputValueUserControl.BindControl(param);
+                actionInputValueUserControl.Margin = new Thickness(5);
+                ActionConfigGrid.Children.Add(actionInputValueUserControl);
+                Grid.SetRow(actionInputValueUserControl, rnum);
+                Grid.SetColumn(actionInputValueUserControl, 1);
+                rnum++;
             }
         }
     }
