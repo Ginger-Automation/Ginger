43b8219b1086f948343b2c24707cec35fd2148d6
Yaron Weiss <yaron.weiss@amdocs.com>
2018-10-22 15:26:52 -0500

    GingerNode support for List<T>

--- Ginger/GingerCoreCommon/Actions/ActionInputValueInfo.cs
+++ Ginger/GingerCoreCommon/Actions/ActionInputValueInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using Amdocs.Ginger.Plugin.Core;
 using Newtonsoft.Json;
@@ -24,6 +25,16 @@ namespace Amdocs.Ginger.Common.Actions
                 if (ParamType == typeof(Int32)) return "int";
                 if (ParamType == typeof(List<string>)) return "List<string>";
                 if (ParamType == typeof(IGingerAction)) return "IGingerAction";
+
+
+                // Check if it is a List 
+                if (ParamType.IsGenericType && ParamType.GetGenericTypeDefinition() == typeof(List<>))
+                {
+                    Type itemType = ParamType.GetGenericArguments()[0];
+                    string listTypeInfo = "List<" + itemType.FullName + ">" + GetListItemProperties(itemType);
+                    return listTypeInfo;
+                }
+
                 //return ParamType.Name;
                 throw new Exception("Unknown param type to handle: " + ParamType.FullName);
             }
@@ -45,6 +56,19 @@ namespace Amdocs.Ginger.Common.Actions
             }
         }
 
+        private string GetListItemProperties(Type itemType)
+        {
+            string s = "";
+            foreach(PropertyInfo PI in itemType.GetProperties())
+            {
+                if (s.Length > 0) s += ",";
+                s += PI.Name + ":" + PI.PropertyType.Name;
+            }            
+            return "{Properties=" + s + "}" ;
+        }
+
         public Type ParamType { get; set; }
+        
+        
     }
 }
