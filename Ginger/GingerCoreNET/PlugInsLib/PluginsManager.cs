6a0db023863e53374b4d3899b1cd13c7858f3f6e
Yaron Weiss <yaron.weiss@amdocs.com>
2018-11-17 21:08:08 -0600

    PluginsManager cleanup

--- Ginger/GingerCoreNET/PlugInsLib/PluginsManager.cs
+++ Ginger/GingerCoreNET/PlugInsLib/PluginsManager.cs
@@ -26,11 +26,8 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Net.Http;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Amdocs.Ginger.Repository
 {
@@ -51,67 +48,7 @@ namespace Amdocs.Ginger.Repository
             public string PluginPackageFolder { get; set; }
         }
 
-        public List<DriverInfo> GetAllDrivers()
-        {
-            //TODO: cache !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            List<DriverInfo> drivers = new List<DriverInfo>();
-
-            foreach (PluginPackage p in mPluginPackages)
-            {
-                //foreach (string s in p.GetDrivers())
-                //{
-                //    DriverInfo di = new DriverInfo();
-                //    di.Name = s;
-                //    di.PluginPackageFolder = p.Folder;
-                //    drivers.Add(di);
-                //}
-            }
-            return drivers;
-        }
-
-        //public List<GingerAction> GetDriverActions(DriverInfo DI)
-        //{
-        //    List<GingerAction> actions = new List<GingerAction>();
-        //    PluginPackage p = (from x in mPluginPackages where x.Folder == DI.PluginPackageFolder select x).FirstOrDefault();
-        //    PluginDriverBase driver = p.GetDriver(DI.Name);
-        //    foreach (ActionHandler AH in driver.ActionHandlers)
-        //    {
-        //        GingerAction GA = new GingerAction(AH.ID);
-        //        UpdateActionParamTypes(GA, AH.MethodInfo);
-        //        actions.Add(GA);
-        //    }
-        //    return actions;
-        //}
-
-        ObservableList<StandAloneAction> list;
-        public ObservableList<StandAloneAction> GetStandAloneActions()
-        {
-            if (list == null)
-            {
-                list = new ObservableList<StandAloneAction>();
-                //foreach (PluginPackage p in mPluginPackages)
-                //{
-                //    foreach (StandAloneAction SAA in p.GetStandAloneActions())
-                //    {
-                //        list.Add(SAA);
-                //    }
-                //}
-            }
-            return list;
-        }
-
-        //private void UpdateActionParamTypes(GingerAction gA, MethodInfo methodInfo)
-        //{
-        //    foreach (ParameterInfo PI in methodInfo.GetParameters())
-        //    {
-        //        if (PI.ParameterType != typeof(GingerAction))
-        //        {
-        //            ActionParam AP = gA.InputParams.GetOrCreateParam(PI.Name);
-        //            AP.ParamType = PI.ParameterType;
-        //        }
-        //    }
-        //}
-
+      
         public void AddPluginPackage(string folder)
         {
             // Verify folder exist
@@ -130,96 +67,13 @@ namespace Amdocs.Ginger.Repository
             Console.WriteLine(s);
         }
 
-        static Assembly LoadFromSameFolder(object sender, ResolveEventArgs args)
+
+        public string InstallPluginPackage(OnlinePluginPackage onlinePluginPackage, OnlinePluginPackageRelease release)
         {
-            string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
-            if (File.Exists(assemblyPath))
-            {
-                Assembly assembly = Assembly.LoadFrom(assemblyPath);
-
-
-                return assembly;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public string InstallPluginPackage(OnlinePluginPackage currentItem, OnlinePluginPackageRelease release)
-        {
-            string folder = currentItem.InstallPluginPackage(release);
+            string folder = onlinePluginPackage.InstallPluginPackage(release);
             AddPluginPackage(folder);
             return folder;
         }
-
-        internal DriverInfo GetDriverInfo(string PluginDriverName)
-        {
-            foreach (DriverInfo di in GetAllDrivers())
-            {
-                if (di.Name == PluginDriverName)
-                {
-                    return di;
-                }
-            }
-            return null;
-        }
-
-        //internal ActionHandler GetStandAloneActionHandler(string pluginID, string ID)
-        //{
-        //    foreach (PluginPackage PP in mPluginPackages)
-        //    {
-        //        PP.ScanPackage();
-        //        ActionHandler AH = PP.GetStandAloneActionHandler(ID);
-        //        if (AH != null)
-        //        {
-        //            return AH;
-        //        }
-        //    }
-        //    throw new Exception("Action handler not found for Action ID: " + ID);
-        //}
-
-        // static List<PluginPackage> mInstalledPluginPackages = null;
-
-       
-
-        // Get list of installed plugins in Ginger folder 'PluginPackages'
-        //public List<PluginPackage> GetInstalledPluginPackages()
-        //{
-        //    if (mInstalledPluginPackages != null)
-        //    {
-        //        //TODO: check for new added plugins
-
-        //        return mInstalledPluginPackages;
-        //    }
-
-        //    mInstalledPluginPackages = new List<PluginPackage>();
-
-        //    string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-        //    if (path.Contains("GingerWPF"))   // We are running from GingerWPF in debug mode
-        //    {
-        //        path = path.Replace(@"GingerWPF\bin\Debug", "");   // temp need to be Ginger installation folder !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        //    }
-
-        //    if (path.Contains("GingerCoreNETUnitTest"))   // We are running from GingerWPF in debug mode
-        //    {
-        //        path = path.Replace(@"GingerCoreNETUnitTest\bin\Debug\netcoreapp2.0", "");
-        //    }
-
-        //    string pluginPackagesPath = Path.Combine(path, "PluginPackages");
-
-        //    // Each directory is a plugin package
-
-        //    foreach (string d in Directory.GetDirectories(pluginPackagesPath))
-        //    {
-        //        PluginPackage p = new PluginPackage(d);
-        //        mInstalledPluginPackages.Add(p);
-        //    }
-        //    return mInstalledPluginPackages;
-        //}
-
 
 
         public string CreatePluginPackageInfo(string id, string version)
