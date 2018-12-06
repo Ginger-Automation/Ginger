0aafc42e2787a3637cf8c4e51bb6bbcbd4fad247
eliranba <eliran.bardavid@amdocs.com>
2018-12-06 12:15:25 +0200

    Adding API and POM learning unit tests

---
+++ Ginger/Unit Tests/NonUITests/AutoPilot/APILearningTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GingerWPF.ApplicationModelsLib.APIModels;
+using Amdocs.Ginger.Common;
+using Amdocs.Ginger.Repository;
+using System.Threading.Tasks;
+using GingerTestHelper;
+using Amdocs.Ginger.Common.Repository.ApplicationModelLib;
+using Ginger.ApplicationModelsLib.ModelOptionalValue;
+using Amdocs.Ginger.Common.APIModelLib;
+using Amdocs.Ginger.Common.Repository.ApplicationModelLib.APIModelLib;
+
+namespace UnitTests.NonUITests.AutoPilot
+{
+    /// <summary>
+    /// Summary description for APILearningTests
+    /// </summary>
+    [TestClass]
+    public class APILearningTests
+    {
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void APILearnWSDLStockSimbolTest()
+        {
+            //Arrange
+            WSDLParser wsdlParser = new WSDLParser();
+            ObservableList<ApplicationAPIModel> AAMSList = new ObservableList<ApplicationAPIModel>();
+
+            //Act
+            Learn(AAMSList, wsdlParser);
+            while (AAMSList.Count < 6)
+            {
+                //Let it learn
+            }
+
+
+            //Assert
+            Assert.AreEqual(AAMSList.Count, 6, "Is API's equal to 6");
+            Assert.AreEqual(AAMSList[0].EndpointURL, "http://ws.cdyne.com/delayedstockquote/delayedstockquote.asmx", "Is EndpointURL equal");
+            Assert.AreEqual(AAMSList[0].SOAPAction, "http://ws.cdyne.com/GetQuote", "Is SOAPAction equal");
+            Assert.AreEqual(String.IsNullOrEmpty(AAMSList[0].RequestBody), false, "Is body not empty equal");
+            Assert.AreEqual(AAMSList[0].AppModelParameters.Count, 2, "are parameters are equal");
+            Assert.AreEqual(AAMSList[0].AppModelParameters[0].PlaceHolder, "{STOCKSYMBOL}", "Is parameter name equal");
+        }
+
+        private async void Learn(ObservableList<ApplicationAPIModel> AAMSList, WSDLParser wsdlParser)
+        {
+            string path = TestResources.GetTestResourcesFile(@"AutoPilot\WSDLs\delayedstockquote.xml");
+            await Task.Run(() => wsdlParser.ParseDocument(path, AAMSList));
+        }
+
+        [TestMethod]
+        public void XMLTemplateParesrCreatePaymentProfile()
+        {
+            //Arrange                        
+            XMLTemplateParser xMLTemplateParser = new XMLTemplateParser();
+            string createPaymentProfileFileName = TestResources.GetTestResourcesFile(@"AutoPilot\XMLTemplates\bankCode.xml");
+
+            //Act   
+            ObservableList<ApplicationAPIModel> createPaymentProfileModels = new ObservableList<ApplicationAPIModel>();
+            xMLTemplateParser.ParseDocument(createPaymentProfileFileName, createPaymentProfileModels);
+            TemplateFile TempleteFile = new TemplateFile() { FilePath = createPaymentProfileFileName };
+            createPaymentProfileModels[0].OptionalValuesTemplates.Add(TempleteFile);
+            Dictionary<Tuple<string, string>, List<string>> OptionalValuesPerParameterDict = new Dictionary<Tuple<string, string>, List<string>>();
+            ImportOptionalValuesForParameters ImportOptionalValues = new ImportOptionalValuesForParameters();
+            ImportOptionalValues.ShowMessage = false;
+            ImportOptionalValues.GetAllOptionalValuesFromExamplesFiles(createPaymentProfileModels[0], OptionalValuesPerParameterDict);
+            ImportOptionalValues.PopulateOptionalValuesForAPIParameters(createPaymentProfileModels[0], OptionalValuesPerParameterDict);
+
+            //Assert
+            Assert.AreEqual(createPaymentProfileModels.Count, 1, "APIModels count");
+            Assert.AreEqual(createPaymentProfileModels[0].AppModelParameters.Count, 1, "AppModelParameters count");
+            Assert.AreEqual(createPaymentProfileModels[0].AppModelParameters[0].OptionalValuesList.Count, 1, "AppModelParameters count");
+        }
+
+        [TestMethod]
+        public void JSONTemplateCreateBillingArrangement()
+        {
+            //Arrange                        
+            JSONTemplateParser jSONTemplateParser = new JSONTemplateParser();
+            string createBillingArrangementFileName = TestResources.GetTestResourcesFile(@"AutoPilot\JSONTemplates\login.json");
+
+            //Act   
+            ObservableList<ApplicationAPIModel> createPaymentProfileModels = new ObservableList<ApplicationAPIModel>();
+            jSONTemplateParser.ParseDocument(createBillingArrangementFileName, createPaymentProfileModels);
+            TemplateFile TempleteFile = new TemplateFile() { FilePath = createBillingArrangementFileName };
+            createPaymentProfileModels[0].OptionalValuesTemplates.Add(TempleteFile);
+            Dictionary<Tuple<string, string>, List<string>> OptionalValuesPerParameterDict = new Dictionary<Tuple<string, string>, List<string>>();
+            ImportOptionalValuesForParameters ImportOptionalValues = new ImportOptionalValuesForParameters();
+            ImportOptionalValues.ShowMessage = false;
+            ImportOptionalValues.GetAllOptionalValuesFromExamplesFiles(createPaymentProfileModels[0], OptionalValuesPerParameterDict);
+            ImportOptionalValues.PopulateOptionalValuesForAPIParameters(createPaymentProfileModels[0], OptionalValuesPerParameterDict);
+
+            //Assert
+            Assert.AreEqual(createPaymentProfileModels.Count, 1, "APIModels count");
+            Assert.AreEqual(createPaymentProfileModels[0].AppModelParameters.Count, 2, "AppModelParameters count");
+            Assert.AreEqual(createPaymentProfileModels[0].AppModelParameters[0].OptionalValuesList.Count, 1, "AppModelParameters count");
+        }
+
+        [TestMethod]
+        public void APILearnSwaggerMetroTest()
+        {
+            //Arrange
+            SwaggerParser wsdlParser = new SwaggerParser();
+            ObservableList<ApplicationAPIModel> AAMSList = new ObservableList<ApplicationAPIModel>();
+
+            //Act
+            string path = TestResources.GetTestResourcesFile(@"AutoPilot\Swagger\swagger.json");
+            wsdlParser.ParseDocument(path, AAMSList);
+
+
+            //Assert
+            Assert.AreEqual(AAMSList.Count, 4, "Is API's equal to 6");
+            Assert.AreEqual(AAMSList[0].EndpointURL, "illin3000:4009//getFutureTransactionDetails", "Is EndpointURL equal");
+            Assert.AreEqual(AAMSList[0].SOAPAction, "http://ws.cdyne.com/GetQuote", "Is SOAPAction equal");
+            Assert.AreEqual(String.IsNullOrEmpty(AAMSList[0].RequestBody), false, "Is body not empty equal");
+            Assert.AreEqual(AAMSList[0].AppModelParameters.Count, 2, "are parameters are equal");
+            Assert.AreEqual(AAMSList[0].AppModelParameters[0].PlaceHolder, "{STOCKSYMBOL}", "Is parameter name equal");
+        }
+    }
+}
---
+++ Ginger/Unit Tests/NonUITests/AutoPilot/POMLearningTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GingerCore;
+using GingerCore.Drivers;
+using Amdocs.Ginger.Common;
+using Amdocs.Ginger.Common.UIElement;
+using GingerCore.Actions.Common;
+using GingerCore.Actions;
+using System.Threading.Tasks;
+
+namespace UnitTests.NonUITests.AutoPilot
+{
+    /// <summary>
+    /// Summary description for POMLearning
+    /// </summary>
+    [TestClass]
+    public class POMLearningTests
+    {
+
+        [TestMethod]
+        public void POMLearnFacebookPageTest()
+        {
+            //Arrange
+            ObservableList<ElementInfo> mElementsList = new ObservableList<ElementInfo>();
+            SeleniumDriver seleniumDriver = new SeleniumDriver(GingerCore.Drivers.SeleniumDriver.eBrowserType.Chrome);
+            seleniumDriver.StartDriver();
+            seleniumDriver.RunAction(new ActBrowserElement() { ControlAction = ActBrowserElement.eControlAction.GotoURL, ValueForDriver = "http://www.facebook.com" });
+            //Act
+            Learn(seleniumDriver, mElementsList);
+            while (mElementsList.Count < 30)
+            {
+                //Let it learn
+            }
+            seleniumDriver.mStopProcess = true;
+
+            while (seleniumDriver.IsDriverBusy)
+            {
+                //Let it stop
+            }
+
+            //Assert
+            Assert.AreNotEqual(mElementsList.Count, 0, "Is Elements Empty List");
+            Assert.IsFalse(String.IsNullOrEmpty(mElementsList[10].XPath));
+            Assert.AreNotEqual(mElementsList[11].Locators.Count,0, "Is Locators Empty  List");
+            Assert.AreNotEqual(mElementsList[12].Properties.Count, 0, "Is Properties Empty  List");
+        }
+
+        private async void Learn(SeleniumDriver seleniumDriver, ObservableList<ElementInfo> mElementsList)
+        {
+            await Task.Run(() => ((IWindowExplorer)seleniumDriver).GetVisibleControls(null, mElementsList, true));
+        }
+    }
+}
