95495afbb1a8871fa207b2c86c55ba8c92f75493
Yaron Weiss <yaron.weiss@amdocs.com>
2018-12-21 17:06:23 -0600

    BF Platforms back for backword compatibility with old XML having patforms

--- Ginger/GingerCoreCommon/Repository/BusinessFlowLib/BusinessFlow.cs
+++ Ginger/GingerCoreCommon/Repository/BusinessFlowLib/BusinessFlow.cs
@@ -107,6 +107,7 @@ namespace GingerCore
             public static string Source = "Source";
         }
 
+        public object Platforms { get; set; } // keep it for backword compatibility when loading old XML, or handle in RI serializer
 
         public List<string> VariablesBeforeExec { get; set; }
 
