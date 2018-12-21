e2b31353d6aba4087286e17c602d4c508f3c6a41
Yaron Weiss <yaron.weiss@amdocs.com>
2018-12-21 11:36:25 -0600

    SolutionMenu with list BF working

--- Ginger/GingerConsole/Menus/SolutionMenu.cs
+++ Ginger/GingerConsole/Menus/SolutionMenu.cs
@@ -16,10 +16,17 @@ limitations under the License.
 */
 #endregion
 
+using amdocs.ginger.GingerCoreNET;
 using Amdocs.Ginger.Common;
 using Amdocs.Ginger.Repository;
-//using GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.BusinessFlowLib;
-//using GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.EnvironmentsLib;
+using Ginger.Reports;
+using Ginger.Run;
+using GingerCore;
+using GingerCore.Actions;
+using GingerCore.Activities;
+using GingerCore.DataSource;
+using GingerCore.Environments;
+using GingerCore.Variables;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -54,8 +61,71 @@ namespace Amdocs.Ginger.GingerConsole
             return SolutionMenu;
         }
 
+        public static void InitClassTypesDictionary()
+        {
+            //TODO: cleanup after all RIs moved to GingerCoreCommon
+
+            //if (bDone) return;
+            //bDone = true;
+
+            // TODO: remove after we don't need old serializer to load old repo items
+            // NewRepositorySerializer.NewRepositorySerializerEvent += RepositorySerializer.NewRepositorySerializer_NewRepositorySerializerEvent;
+
+            // Add all RI classes from GingerCoreCommon
+            NewRepositorySerializer.AddClassesFromAssembly(typeof(RepositoryItemBase).Assembly);
+
+            // Add all RI classes from GingerCore
+            // NewRepositorySerializer.AddClassesFromAssembly(typeof(GingerCore.Actions.ActButton).Assembly); // GingerCore.dll
+
+            // add  old Plugins - TODO: remove later when we change to new plugins
+            // NewRepositorySerializer.AddClassesFromAssembly(typeof(GingerPlugIns.ActionsLib.PlugInActionsBase).Assembly);
+
+
+            // add from Ginger - items like RunSetConfig
+            // NewRepositorySerializer.AddClassesFromAssembly(typeof(Ginger.App).Assembly);
+
+            // Each class which moved from GingerCore to GingerCoreCommon needed to be added here, so it will auto translate
+            // For backward compatibility of loading old object name in xml
+            Dictionary<string, Type> list = new Dictionary<string, Type>();
+            list.Add("GingerCore.Actions.ActInputValue", typeof(ActInputValue));
+            list.Add("GingerCore.Actions.ActReturnValue", typeof(ActReturnValue));
+            list.Add("GingerCore.Actions.EnhancedActInputValue", typeof(EnhancedActInputValue));
+            list.Add("GingerCore.Environments.GeneralParam", typeof(GeneralParam));
+
+            // Put back for Lazy load of BF.Acitvities
+            NewRepositorySerializer.AddLazyLoadAttr(nameof(BusinessFlow.Activities)); // TODO: add RI type, and use attr on field
+
+
+            // Verify the old name used in XML
+            //list.Add("GingerCore.Actions.RepositoryItemTag", typeof(RepositoryItemTag));
+            //list.Add("GingerCore.Actions.EnhancedActInputValue", typeof(EnhancedActInputValue));
+
+            // TODO: change to SR2  if we want the files to be loaded convert and save with the new SR2
+
+            //if (WorkSpace.Instance.BetaFeatures.UseNewRepositorySerializer)
+            //{
+            //RepositorySerializer2 RS2 = new RepositorySerializer2();
+
+            //SolutionRepository.mRepositorySerializer = RS2;
+            //RepositoryFolderBase.mRepositorySerializer = RS2;
+            //    ObservableListSerializer.RepositorySerializer = RS2;
+
+            //}
+            //else
+            //{
+            //        SolutionRepository.mRepositorySerializer = new RepositorySerializer();
+            //        RepositoryFolderBase.mRepositorySerializer = new RepositorySerializer();
+            //}
+
+            NewRepositorySerializer.AddClasses(list);
+
+        }
+
+
         private void OpenSolution()
         {
+            InitClassTypesDictionary();
+
             Console.WriteLine("Solution Folder?");
             string sFolder = Console.ReadLine();
             if (Directory.Exists(sFolder))
@@ -64,6 +134,35 @@ namespace Amdocs.Ginger.GingerConsole
                 Console.WriteLine("Opening Solution at folder: " + sFolder);
 
                 SR = new SolutionRepository();
+                SR.AddItemInfo<BusinessFlow>("*.Ginger.BusinessFlow.xml", @"~\BusinessFlows", true, GingerDicser.GetTermResValue(eTermResKey.BusinessFlows), PropertyNameForFileName: nameof(BusinessFlow.Name));
+
+                SR.AddItemInfo<ApplicationAPIModel>("*.Ginger.ApplicationAPIModel.xml", @"~\Applications Models\API Models", true, "API Models", PropertyNameForFileName: nameof(ApplicationAPIModel.Name));
+                SR.AddItemInfo<GlobalAppModelParameter>("*.Ginger.GlobalAppModelParameter.xml", @"~\Applications Models\Global Models Parameters", true, "Global Model Parameters", PropertyNameForFileName: nameof(GlobalAppModelParameter.PlaceHolder));
+                SR.AddItemInfo<ApplicationPOMModel>("*.Ginger.ApplicationPOMModel.xml", @"~\Applications Models\POM Models", true, "POM Models", PropertyNameForFileName: nameof(ApplicationPOMModel.Name));
+
+                SR.AddItemInfo<ProjEnvironment>("*.Ginger.Environment.xml", @"~\Environments", true, "Environments", PropertyNameForFileName: nameof(ProjEnvironment.Name));
+                SR.AddItemInfo<ALMDefectProfile>("*.Ginger.ALMDefectProfile.xml", @"~\ALMDefectProfiles", true, "ALM Defect Profiles", PropertyNameForFileName: nameof(ALMDefectProfile.Name));
+
+                // SR.AddItemInfo<Agent>("*.Ginger.Agent.xml", @"~\Agents", true, "Agents", PropertyNameForFileName: nameof(Agent.Name));
+
+                //TODO: check if below 2 reports folders are realy needed
+                SR.AddItemInfo<HTMLReportConfiguration>("*.Ginger.HTMLReportConfiguration.xml", @"~\HTMLReportConfigurations", true, "HTMLReportConfigurations", PropertyNameForFileName: nameof(HTMLReportsConfiguration.Name));
+                SR.AddItemInfo<HTMLReportTemplate>("*.Ginger.HTMLReportTemplate.xml", @"~\HTMLReportConfigurations\HTMLReportTemplate", true, "HTMLReportTemplate", PropertyNameForFileName: nameof(HTMLReportTemplate.Name));
+
+                // SR.AddItemInfo<ReportTemplate>("*.Ginger.ReportTemplate.xml", @"~\HTMLReportConfigurations\ReportTemplates", true, "ReportTemplates", PropertyNameForFileName: nameof(ReportTemplate.Name));
+
+                SR.AddItemInfo<DataSourceBase>("*.Ginger.DataSource.xml", @"~\DataSources", true, "Data Sources", PropertyNameForFileName: nameof(DataSourceBase.Name));
+
+                SR.AddItemInfo<PluginPackage>("*.Ginger.PluginPackage.xml", @"~\Plugins", true, "Plugins", PropertyNameForFileName: nameof(PluginPackage.PluginId));
+
+                SR.AddItemInfo<ActivitiesGroup>("*.Ginger.ActivitiesGroup.xml", @"~\SharedRepository\ActivitiesGroup", true, GingerDicser.GetTermResValue(eTermResKey.ActivitiesGroups, "Shared "), PropertyNameForFileName: nameof(ActivitiesGroup.Name));
+                SR.AddItemInfo<Activity>("*.Ginger.Activity.xml", @"~\SharedRepository\Activities", true, GingerDicser.GetTermResValue(eTermResKey.Activities, "Shared "), PropertyNameForFileName: nameof(Activity.ActivityName));
+                SR.AddItemInfo<Act>("*.Ginger.Action.xml", @"~\SharedRepository\Actions", true, "Shared Actions", PropertyNameForFileName: nameof(Act.Description));
+                SR.AddItemInfo<VariableBase>("*.Ginger.Variable.xml", @"~\SharedRepository\Variables", true, GingerDicser.GetTermResValue(eTermResKey.Variables, "Shared "), PropertyNameForFileName: nameof(VariableBase.Name));
+
+                SR.AddItemInfo<RunSetConfig>("*.Ginger.RunSetConfig.xml", @"~\RunSetConfigs", true, GingerDicser.GetTermResValue(eTermResKey.RunSets), PropertyNameForFileName: nameof(RunSetConfig.Name));
+
+                WorkSpace.Instance.SolutionRepository = SR;
                 SR.Open(sFolder);
                 IEnumerable<RepositoryFile> files = SR.GetAllSolutionRepositoryFiles().ToList();
                 Console.WriteLine("Files count = " + files.Count());
@@ -84,9 +183,9 @@ namespace Amdocs.Ginger.GingerConsole
 
         private void BusinessFlowsCount()
         {
-            //Console.WriteLine("Executing BFs count");
-            //ObservableList<BusinessFlow> BFs = SR.GetAllRepositoryItems<BusinessFlow>();
-            //Console.WriteLine("BFs count = " + BFs.Count());
+            Console.WriteLine("Executing BFs count");
+            ObservableList<BusinessFlow> BFs = SR.GetAllRepositoryItems<BusinessFlow>();
+            Console.WriteLine("BFs count = " + BFs.Count());
         }
 
         private void CloseSolution()
@@ -101,14 +200,19 @@ namespace Amdocs.Ginger.GingerConsole
 
         private void BusinessFlowsList()
         {
-            //Console.WriteLine("Executing BFs List");
-            //ObservableList<BusinessFlow> BFs = SR.GetAllRepositoryItems<BusinessFlow>();
-            //int count = 0;
-            //foreach (BusinessFlow BF in BFs)
-            //{
-            //    count++;
-            //    Console.WriteLine("# " + count + " - " + BF.Name);
-            //}
+            Console.WriteLine("Executing BFs List");
+            ObservableList<BusinessFlow> BFs = SR.GetAllRepositoryItems<BusinessFlow>();
+            int count = 0;
+            foreach (BusinessFlow BF in BFs)
+            {
+                count++;
+                Console.WriteLine("# " + count + " - " + BF.Name);
+                foreach (BusinessFlow BF in BFs)
+                {
+                    count++;
+                    Console.WriteLine("# " + count + " - " + BF.Name);
+                }
+            }
         }
 
         void EnvironmentsList()
