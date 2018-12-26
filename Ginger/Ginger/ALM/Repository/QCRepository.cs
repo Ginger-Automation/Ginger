7da5bedc89ad861f0b56953836f4c47464b528e8
Haim Uzan <uzanhaim@gmail.com>
2018-12-26 12:27:54 +0200

    fix test case list filter

--- Ginger/Ginger/ALM/Repository/QCRepository.cs
+++ Ginger/Ginger/ALM/Repository/QCRepository.cs
@@ -290,15 +290,11 @@ namespace Ginger.ALM.Repository
             ALMIntegration.Instance.RefreshALMItemFields(testCaseFields, true, null);
 
             //Going through the fields to leave only Test Case fields
-            for (int indx = 0; indx < testCaseFields.Count; indx++)
+            for (int indx = testCaseFields.Count - 1; indx >= 0; indx--)
             {
-                if(testCaseFields[indx].Name == "Version")
-                {
-                }
                 if (testCaseFields[indx].ItemType != eQCItemType.TestCase.ToString())
                 {
                     testCaseFields.RemoveAt(indx);
-                    indx--;
                 }
             }
             
