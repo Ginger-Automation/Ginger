e40cfee96b1b8985e12a1e3d39107a931d58ac68
Yaron Weiss <yaron.weiss@amdocs.com>
2019-01-11 22:59:20 -0600

    AddGlobalStringVariable temp Ignore for unit test

--- Ginger/GingerTest/POMs/Common/ucGridPOM.cs
+++ Ginger/GingerTest/POMs/Common/ucGridPOM.cs
@@ -1,13 +1,7 @@
-using Amdocs.Ginger.Common.UIElement;
 using Amdocs.Ginger.UserControls;
 using Ginger;
 using GingerWPFUnitTest.POMs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
