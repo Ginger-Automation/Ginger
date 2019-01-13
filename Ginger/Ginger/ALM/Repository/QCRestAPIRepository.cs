7307f0748cd931640a4d81f5ba6d017a74c5ce6c
Meni Kadosh <MENIK@amdocs.com>
2019-01-13 19:36:38 +0200

    fixing build issues

--- Ginger/Ginger/ALM/Repository/QCRestAPIRepository.cs
+++ Ginger/Ginger/ALM/Repository/QCRestAPIRepository.cs
@@ -392,7 +392,7 @@ namespace Ginger.ALM.Repository
 
         public override eUserMsgKey GetDownloadPossibleValuesMessage()
         {
-            return eUserMsgKeys.AskIfToDownloadPossibleValuesShortProcesss;
+            return eUserMsgKey.AskIfToDownloadPossibleValuesShortProcesss;
         }
 
         public override bool LoadALMConfigurations()
