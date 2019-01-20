dc9ece372ed34921e5e88335f55e115783440b75
Yaron Weiss <yaron.weiss@amdocs.com>
2019-01-20 17:50:01 -0600

    Fix remove runner in Run set

---
+++ Ginger/Ginger/GeneralWindows/Run Set Pages/RunnerPageEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ginger.Run
+{
+    public class RunnerPageEventArgs
+    {
+        public enum eEventType
+        {
+            RemoveRunner,
+            DuplicateRunner,
+            ResetRunnerStatus
+        }
+
+        public eEventType EventType;
+        public Object Object;
+
+        public RunnerPageEventArgs(eEventType EventType, object Object)
+        {
+            this.EventType = EventType;
+            this.Object = Object;
+        }
+    }
+}
