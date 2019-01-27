e6d2db201eeb5ed0a4692b38f831036013b06546
Yaron Weiss <yaron.weiss@amdocs.com>
2019-01-27 16:09:41 -0600

    GingerWeb - runset controller to run runset + fix for agents loading

--- Ginger/GingerWeb/Controllers/RunSetController.cs
+++ Ginger/GingerWeb/Controllers/RunSetController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using amdocs.ginger.GingerCoreNET;
 using Ginger.Run;
 using GingerWeb.UsersLib;
 using Microsoft.AspNetCore.Mvc;
@@ -28,32 +29,51 @@ namespace GingerWeb.Controllers
         }
 
 
-        //[HttpPost("[action]")]
-        //public RunBusinessFlowResult RunBusinessFlow([FromBody] RunBusinessFlowRequest runBusinessFlowRequest)
-        //{
-        //    RunBusinessFlowResult runBusinessFlowResult = new RunBusinessFlowResult();
+        public class RunRunSetResult
+        {
+            public string name { get; set; }
+            public string Status { get; internal set; }
+        }
 
-        //    if (string.IsNullOrEmpty(runBusinessFlowRequest.name))
-        //    {
-        //        runBusinessFlowResult.Status = "Name cannot be null";
-        //        return runBusinessFlowResult;
-        //    }
-
-        //    BusinessFlow BF = (from x in General.SR.GetAllRepositoryItems<BusinessFlow>() where x.Name == runBusinessFlowRequest.name select x).SingleOrDefault();
-        //    if (BF == null)
-        //    {
-        //        runBusinessFlowResult.Status = "Name cannot be null";
-        //        return runBusinessFlowResult;
-        //    }
-
-        //    RunFlow(BF);
-
-        //    runBusinessFlowResult.Status = "Executed - BF.Status=" + BF.RunStatus;
+        public class RunRunSetRequest
+        {
+            public string name { get; set; }
+        }
 
 
+        [HttpPost("[action]")]
+        public RunRunSetResult RunRunSet([FromBody] RunRunSetRequest runRunSetRequest)
+        {
+            RunRunSetResult runBusinessFlowResult = new RunRunSetResult();
 
-        //    return runBusinessFlowResult;
-        //}
+            if (string.IsNullOrEmpty(runRunSetRequest.name))
+            {
+                runBusinessFlowResult.Status = "Name cannot be null";
+                return runBusinessFlowResult;
+            }
 
+            RunSetConfig runSet = (from x in General.SR.GetAllRepositoryItems<RunSetConfig>() where x.Name == runRunSetRequest.name select x).SingleOrDefault();
+            if (runSet == null)
+            {
+                runBusinessFlowResult.Status = "Name cannot be null";
+                return runBusinessFlowResult;
+            }
+
+            RunrunSet(runSet);
+
+            runBusinessFlowResult.Status = "Executed";
+
+
+
+            return runBusinessFlowResult;
+        }
+
+        private void RunrunSet(RunSetConfig runSetConfig)
+        {
+            RunsetExecutor runsetExecutor = new RunsetExecutor();
+            runsetExecutor.RunSetConfig = runSetConfig;
+            WorkSpace.RunsetExecutor = runsetExecutor;
+            runsetExecutor.RunRunset();
+        }
     }
 }
