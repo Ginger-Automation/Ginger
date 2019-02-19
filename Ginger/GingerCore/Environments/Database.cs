677d2730c18e11fed31b580709714cd87e1d9853
Meni Kadosh <MENIK@amdocs.com>
2019-02-19 22:07:07 +0200

    handling un needed decrypt failure log write

--- Ginger/GingerCore/Environments/Database.cs
+++ Ginger/GingerCore/Environments/Database.cs
@@ -227,8 +227,8 @@ namespace GingerCore.Environments
             if (String.IsNullOrEmpty(ConnectionStringCalculated) == false)
             {
                 connStr = ConnectionStringCalculated.Replace("{USER}", UserCalculated);
-
-                String deCryptValue = EncryptionHandler.DecryptString(PassCalculated, ref res);
+     
+                String deCryptValue = EncryptionHandler.DecryptString(PassCalculated, ref res, false);
                 if (res == true)
                     { connStr = connStr.Replace("{PASS}", deCryptValue); }
                 else
@@ -240,7 +240,7 @@ namespace GingerCore.Environments
                 String strProvider;
                 connStr = "Data Source=" + TNSCalculated + ";User Id=" + UserCalculated + ";";
 
-                String deCryptValue = EncryptionHandler.DecryptString(PassCalculated, ref res);
+                String deCryptValue = EncryptionHandler.DecryptString(PassCalculated, ref res, false);
 
                 if (res == true) { connStr = connStr + "Password=" + deCryptValue + ";"; }
                 else { connStr = connStr + "Password=" + PassCalculated + ";"; }
