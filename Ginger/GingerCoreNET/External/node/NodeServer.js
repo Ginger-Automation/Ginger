39899966c6fd3555084542f68baa450e3edef80c
Mohd Ambar Khan <MohdAmbar.Khan@amdocs.com>
2019-03-05 12:24:27 +0530

    Revert "Node Js integration tests"
    This reverts commit d39a788010bb5fd4c07c63a416c757614db90455.

--- Ginger/GingerCoreNET/External/node/NodeServer.js
+++
@@ -1,8 +0,0 @@
-﻿var WebSocketServer=require("ws").Server;
-var wss = new WebSocketServer({port:3000});
-
-wss.on("connection",function(ws){
-
-    console.log("Client connected");
-    ws.send("Connected to Ginger Node Js server");
-});
