70d9f2aec53024962aac91340945481ef73c66f3
Meni Kadosh <MENIK@amdocs.com>
2019-04-02 23:22:36 +0300

    Fixing missing env on VE test when running for Agent config (outside Automate tab)

--- Ginger/Ginger/ValueExpression/ValueExpressionEditorPage.xaml.cs
+++ Ginger/Ginger/ValueExpression/ValueExpressionEditorPage.xaml.cs
@@ -18,6 +18,7 @@ limitations under the License.
 
 using amdocs.ginger.GingerCoreNET;
 using Amdocs.Ginger.Common;
+using Amdocs.Ginger.CoreNET.RosLynLib.Refrences;
 using Amdocs.Ginger.CoreNET.ValueExpression;
 using Amdocs.Ginger.Repository;
 using Ginger.Actions;
@@ -35,38 +36,22 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Ginger.DataSource;
-using GingerCore.DataSource;
-using Ginger.Actions;
-using Ginger.UserControlsLib.TextEditor;
-using Ginger.Variables;
-using Ginger.Environments;
-using System.Reflection;
-using Amdocs.Ginger.CoreNET.ValueExpression;
-using Amdocs.Ginger.Repository;
-using amdocs.ginger.GingerCoreNET;
-using Ginger.SolutionGeneral;
-using System.IO;
-using System.Dynamic;
-using Newtonsoft.Json.Linq;
-using Amdocs.Ginger.Common.InterfacesLib;
-using System.Linq;
-using Amdocs.Ginger.CoreNET.RosLynLib.Refrences;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 namespace Ginger
 {
     /// <summary>
     /// Interaction logic for ActionValueEditorWindow.xaml
     /// </summary>
 
-  
+
 
     public partial class ValueExpressionEditorPage : Page
     {
@@ -79,6 +64,7 @@ namespace Ginger
         ValueExpression mVE = null;
         static List<HighlightingRule> mHighlightingRules = null;
         private Dictionary<string, TreeViewItem> Categories = new Dictionary<string, TreeViewItem>();
+        ObservableList<ProjEnvironment> mEnvs;
 
         public ValueExpressionEditorPage(object obj, string AttrName, Context context)
         {
@@ -477,9 +463,9 @@ namespace Ginger
             SetItemView(tviEnvs, "Environments", "", "@Environment_16x16.png");
             xObjectsTreeView.Items.Add(tviEnvs);
 
-            ObservableList<ProjEnvironment> Envs = WorkSpace.Instance.SolutionRepository.GetAllRepositoryItems<ProjEnvironment>();
+            mEnvs = WorkSpace.Instance.SolutionRepository.GetAllRepositoryItems<ProjEnvironment>();
 
-            foreach (ProjEnvironment env in Envs)
+            foreach (ProjEnvironment env in mEnvs)
             {                
                 TreeViewItem tviEnv = new TreeViewItem();
                 SetItemView(tviEnv, env.Name, "", "@Environment_16x16.png");
@@ -766,6 +752,10 @@ namespace Ginger
         {
             if (mVE == null)
             {
+                if (mContext.Environment == null && mEnvs != null && mEnvs.Count > 0)
+                {
+                    mContext.Environment = mEnvs[0];
+                }
                 mVE = new ValueExpression(mContext.Environment, mContext.BusinessFlow, WorkSpace.Instance.SolutionRepository.GetAllRepositoryItems<DataSourceBase>(), false, "", false);
             }
             mVE.Value = this.ValueUCTextEditor.textEditor.Text;
