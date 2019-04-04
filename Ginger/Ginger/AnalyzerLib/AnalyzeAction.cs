de86ea3180963ce541eee520b55819f348b01cc5
Meni Kadosh <MENIK@amdocs.com>
2019-04-04 17:47:11 +0300

    pushing fix for AnalyzerAction showing false issue on BrowserAction with POM mapping + adding try-catch to "~" replacment for Solution path

--- Ginger/Ginger/AnalyzerLib/AnalyzeAction.cs
+++ Ginger/Ginger/AnalyzerLib/AnalyzeAction.cs
@@ -405,7 +405,15 @@ namespace Ginger.AnalyzerLib
             {
                 try
                 {
-                    string[] pOMandElementGUIDs = ((ActUIElement)a).ElementLocateValue.Split('_');
+                    string[] pOMandElementGUIDs;
+                    if (a is ActUIElement)
+                    {
+                        pOMandElementGUIDs = ((ActUIElement)a).ElementLocateValue.Split('_');
+                    }
+                    else
+                    {
+                        pOMandElementGUIDs = a.LocateValue.Split('_');
+                    }
                     Guid selectedPOMGUID = new Guid(pOMandElementGUIDs[0]);
                     ApplicationPOMModel POM = WorkSpace.Instance.SolutionRepository.GetRepositoryItemByGuid<ApplicationPOMModel>(selectedPOMGUID);
                     if (POM == null)
