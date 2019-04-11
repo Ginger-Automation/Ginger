f90eececd105e9f48598ad0c4001e6f894c6247b
Arvind Ghodse <Arvind.Ghodse@amdocs.com>
2019-04-11 17:31:05 +0530

    Added RunsetActionDeliveryMethodConfigPage

---
+++ Ginger/Ginger/Run/RunSetActions/RunSetActionDeliveryMethodConfigPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using GingerCore.GeneralLib;
+using System.Windows;
+using System.Windows.Controls;
+using GingerCore;
+namespace Ginger.Run.RunSetActions
+{
+    /// <summary>
+    /// Interaction logic for RunSetActionDeliveryMethodConfigPage.xaml
+    /// </summary>
+    public partial class RunSetActionDeliveryMethodConfigPage : Page
+    {
+        public RunSetActionDeliveryMethodConfigPage(RunSetActionSendSMS runSetActionSendSMS)
+        {
+            InitializeComponent();
+
+            xSMTPMailHostTextBox.Init(null, runSetActionSendSMS.SMSEmail, nameof(Email.SMTPMailHost));
+            //BindingHandler.ObjFieldBinding(xSMTPMailHostTextBox, TextBox.TextProperty, runSetActionSendSMS.SMSEmail, nameof(Email.SMTPMailHost));
+            BindingHandler.ObjFieldBinding(xSMTPPortTextBox, TextBox.TextProperty, runSetActionSendSMS.SMSEmail, nameof(Email.SMTPPort));
+            //BindingHandler.ObjFieldBinding(xSMTPUserTextBox, TextBox.TextProperty, runSetActionSendSMS.SMSEmail, nameof(Email.SMTPUser));
+            xSMTPUserTextBox.Init(null, runSetActionSendSMS.SMSEmail, nameof(Email.SMTPUser));
+            BindingHandler.ObjFieldBinding(xSMTPPassTextBox, TextBox.TextProperty, runSetActionSendSMS.SMSEmail, nameof(Email.SMTPPass));
+            GingerCore.General.FillComboFromEnumObj(xEmailMethodComboBox, runSetActionSendSMS.SMSEmail.EmailMethod);
+            BindingHandler.ObjFieldBinding(xEmailMethodComboBox, ComboBox.SelectedValueProperty, runSetActionSendSMS.SMSEmail, nameof(Email.EmailMethod));
+        }
+        private void xEmailMethodComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (xEmailMethodComboBox.SelectedItem.ToString() == "OUTLOOK")
+            {
+                xSMTPConfig.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                xSMTPConfig.Visibility = Visibility.Visible;
+            }
+        }
+        private void xcbConfigureCredential_Checked(object sender, RoutedEventArgs e)
+        {
+            xSMTPUserTextBox.Visibility = Visibility.Visible;
+            xSMTPPassTextBox.Visibility = Visibility.Visible;
+            xLabelPass.Visibility = Visibility.Visible;
+            xLabelUser.Visibility = Visibility.Visible;
+        }
+        private void xcbConfigureCredential_Unchecked(object sender, RoutedEventArgs e)
+        {
+            xSMTPUserTextBox.Visibility = Visibility.Collapsed;
+            xSMTPPassTextBox.Visibility = Visibility.Collapsed;
+            xLabelPass.Visibility = Visibility.Collapsed;
+            xLabelUser.Visibility = Visibility.Collapsed;
+        }
+        private void xSMTPPassTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            bool res = false;
+            if (!EncryptionHandler.IsStringEncrypted(xSMTPPassTextBox.Text))
+            {
+                xSMTPPassTextBox.Text = EncryptionHandler.EncryptString(xSMTPPassTextBox.Text, ref res);
+                if (res == false)
+                {
+                    xSMTPPassTextBox.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
