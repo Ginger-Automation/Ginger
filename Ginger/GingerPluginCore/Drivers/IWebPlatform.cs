59d537cc300f3a8062f90b2710ba63b063845efe
Mohd Ambar Khan <MohdAmbar.Khan@amdocs.com>
2019-04-18 22:10:16 +0530

    Adding Action Info classed  In Ginger Plugin COre

--- Ginger/GingerPluginCore/Drivers/IWebPlatform.cs
+++ Ginger/GingerPluginCore/Drivers/IWebPlatform.cs
@@ -1,4 +1,5 @@
-﻿using Amdocs.Ginger.Plugin.Core.PlugInsLib;
+﻿using Amdocs.Ginger.Plugin.Core.ActionsLib;
+using Amdocs.Ginger.Plugin.Core.PlugInsLib;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Amdocs.Ginger.Plugin.Core.Drivers
     [ServiceFeature]
    public interface IWebPlatform
     {
+        void HandleBrowserElementAction(GingerAction GA, ActBrowserInfo act);
     }
 }
