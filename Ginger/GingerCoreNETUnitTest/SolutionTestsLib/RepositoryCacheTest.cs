aa388648fbddbbe392675817300e0e29c57027dc
Yaron Weiss <yaron.weiss@amdocs.com>
2019-05-21 19:11:04 -0500

    RepositoryCacheTest

--- Ginger/GingerCoreNETUnitTest/SolutionTestsLib/RepositoryCacheTest.cs
+++ Ginger/GingerCoreNETUnitTest/SolutionTestsLib/RepositoryCacheTest.cs
@@ -1,115 +1,108 @@
-//#region License
-///*
-//Copyright © 2014-2019 European Support Limited
+#region License
+/*
+Copyright © 2014-2019 European Support Limited
 
-//Licensed under the Apache License, Version 2.0 (the "License")
-//you may not use this file except in compliance with the License.
-//You may obtain a copy of the License at 
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at 
 
-//http://www.apache.org/licenses/LICENSE-2.0 
+http://www.apache.org/licenses/LICENSE-2.0 
 
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS, 
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-//See the License for the specific language governing permissions and 
-//limitations under the License. 
-//*/
-//#endregion
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. 
+*/
+#endregion
 
-//using Amdocs.Ginger.Repository;
-//using GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.BusinessFlowLib;
-//using GingerTestHelper;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Amdocs.Ginger.Repository;
+using GingerCore;
+using GingerTestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
 
-//namespace GingerCoreNETUnitTest.SolutionTestsLib
-//{
-//    [Level1]
-//    [TestClass]
-//    public class RepositoryCacheTest
-//    {
-//        RepositoryCache RC = new RepositoryCache(typeof(BusinessFlow));
+namespace GingerCoreNETUnitTest.SolutionTestsLib
+{
+    [Level1]
+    [TestClass]
+    public class RepositoryCacheTest
+    {
+        RepositoryCache RC = new RepositoryCache(typeof(BusinessFlow));
 
-//        [TestInitialize]
-//        public void TestInitialize()
-//        {
+        [TestInitialize]
+        public void TestInitialize()
+        {
 
-//        }
+        }
 
-//        [TestCleanup]
-//        public void TestCleanUp()
-//        {
+        [TestCleanup]
+        public void TestCleanUp()
+        {
 
-//        }
-
-//        //[TestMethod]  [Timeout(60000)]
-//        //public void WeakReferenceTest()
-//        //{
-//        //    Stopwatch st = new Stopwatch();
-//        //        //BusinessFlow BF = new BusinessFlow();
-//        //    WeakReference WR = new WeakReference(st);
-//        //    st = null;
-//        //    while(WR.IsAlive)
-//        //    {
-//        //        Thread.Sleep(100);
-//        //    }
-
-//        //    Assert.IsTrue(WR.IsAlive == false);            
-//        //}
+        }
         
-//        [TestMethod]  [Timeout(60000)]
-//        public void CachecSimpleInOut()
-//        {
-//            //Arrange            
-//            BusinessFlow BF = new BusinessFlow();
-//            BF.Name = "hello";
 
-//            //Act
-//            RC[BF.Name] = BF;
-//            BusinessFlow BF2 = (BusinessFlow)RC[BF.Name];
+        [TestMethod]
+        [Timeout(60000)]
+        public void CachecSimpleInOut()
+        {
+            //Arrange            
+            BusinessFlow BF = new BusinessFlow();
+            BF.Name = "hello";
 
-//            //Assert            
-//            Assert.AreEqual(BF, BF2); // make sure they are same object/ref
-//        }
+            //Act
+            RC[BF.Name] = BF;
+            BusinessFlow BF2 = (BusinessFlow)RC[BF.Name];
 
-//        //[TestMethod]  [Timeout(60000)]
-//        //public void CachecWithObjRelease()
-//        //{
-//        //    //Arrange            
-//        //    string key = "CachecWithObjRelease";
+            //Assert            
+            Assert.AreEqual(BF, BF2); // make sure they are same object/ref
+        }
 
-//        //    BusinessFlow BF = new BusinessFlow();
-//        //    BF.Name = "My BF 1";
+        [Ignore] // FIXME
+        [TestMethod]
+        [Timeout(60000)]
+        public void CachecWithObjRelease()
+        {
+            //Arrange            
+            string key = "CachecWithObjRelease";
 
-//        //    //Act
-//        //    RC[key] = BF;
-//        //    BF = null; // release
+            BusinessFlow BF = new BusinessFlow();
+            BF.Name = "My BF 1";
+
+            //Act
+            RC[key] = BF;
+            BF = null; // release
             
-//        //    BusinessFlow BF2 = (BusinessFlow)RC[key];
+            BusinessFlow BF2 = (BusinessFlow)RC[key];
 
-//        //    //Assert            
-//        //    Assert.IsTrue(BF2 == null, "BF2 == null"); // make sure obj not found in cache and is released
-//        //}
+            //Assert            
+            Assert.IsTrue(BF2 == null, "BF2 == null"); // make sure obj not found in cache and is released
+        }
 
+        [Ignore] // FIXME
+        [TestMethod]
+        [Timeout(60000)]
+        public void CachecWithObjReleaseAndGCCollect()
+        {
+            //Arrange            
+            string key = "CachecWithObjReleaseAndGCCollect";
 
-//        //[TestMethod]  [Timeout(60000)]
-//        //public void CachecWithObjReleaseAndGCCollect()
-//        //{
-//        //    //Arrange            
-//        //    string key = "CachecWithObjReleaseAndGCCollect";
+            BusinessFlow BF = new BusinessFlow();
+            BF.Name = "My BF 1";
 
-//        //    BusinessFlow BF = new BusinessFlow();
-//        //    BF.Name = "My BF 1";
+            //Act
+            RC[key] = BF;
+            BF = null; // release
 
-//        //    //Act
-//        //    RC[key] = BF;
-//        //    BF = null; // release
+            GC.Collect(); // ((GC.MaxGeneration, GCCollectionMode.Forced);
 
-//        //    GC.Collect(); // ((GC.MaxGeneration, GCCollectionMode.Forced);
+            BusinessFlow BF2 = (BusinessFlow)RC[key];
 
-//        //    BusinessFlow BF2 = (BusinessFlow)RC[key];
+            //Assert            
+            Assert.IsTrue(BF2 == null, "BF2 == null"); // make sure obj not found in cache is released
+        }
 
-//        //    //Assert            
-//        //    Assert.IsTrue(BF2==null, "BF2 == null"); // make sure obj not found in cache is released
-//        //}
-//    }
-//}
+    }
+}
