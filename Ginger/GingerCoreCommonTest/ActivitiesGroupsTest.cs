252e9c368d15b3c49649d212a71d893d2c07cf64
Meni Kadosh <MENIK@amdocs.com>
2019-06-06 15:01:07 +0300

    Adding ActivitiesGroups arrangment unit tests phase 1

---
+++ Ginger/GingerCoreCommonTest/ActivitiesGroupsTest.cs
@@ -0,0 +1,84 @@
+﻿using GingerCore;
+using GingerCore.Activities;
+using GingerTestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GingerCoreCommonTest
+{
+
+    [TestClass]
+    [Level2]
+    public class ActivitiesGroupsTest
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext TC)
+        {
+            //TODO::
+        }
+
+        [TestMethod]
+        public void AddDefualtGroupTest()
+        {
+            //Arrange
+            BusinessFlow busFlow = new BusinessFlow();
+            Activity activity1 = new Activity() { ActivityName = "Activity1" };
+            busFlow.AddActivity(activity1);
+            Activity activity2 = new Activity() { ActivityName = "Activity2" };
+            busFlow.AddActivity(activity2);
+            Activity activity3 = new Activity() { ActivityName = "Activity3" };
+            busFlow.AddActivity(activity3);
+            Activity activity4 = new Activity() { ActivityName = "Activity4" };
+            busFlow.AddActivity(activity4);
+            Activity activity5 = new Activity() { ActivityName = "Activity5" };
+            busFlow.AddActivity(activity5);
+
+            //Act
+            busFlow.AttachActivitiesGroupsAndActivities();
+
+            //Assert
+            Assert.IsTrue(busFlow.ActivitiesGroups.Count == 1, "Validate defualt Activities Group was added");
+            Assert.IsTrue(busFlow.ActivitiesGroups[0].ActivitiesIdentifiers.Count == 5, "Validate all Activities are attached to defualt Activities Group");
+            Assert.IsTrue(busFlow.ActivitiesGroups[0].ActivitiesIdentifiers[2].ActivityName == activity3.ActivityName, "Validate group has correct activities mapped to it");
+            Assert.IsTrue(activity1.ActivitiesGroupID == busFlow.ActivitiesGroups[0].Name, "Validate Activities mapped to defualt group");
+            Assert.IsTrue(activity5.ActivitiesGroupID == busFlow.ActivitiesGroups[0].Name, "Validate Activities mapped to defualt group");
+        }
+
+        [TestMethod]
+        public void GroupUnGroupedActivitiesTest()
+        {
+            //Arrange
+            BusinessFlow busFlow = new BusinessFlow();
+            Activity activity1 = new Activity() { ActivityName = "Activity1" };
+            busFlow.AddActivity(activity1);
+            Activity activity2 = new Activity() { ActivityName = "Activity2" };
+            busFlow.AddActivity(activity2);
+            Activity activity3 = new Activity() { ActivityName = "Activity3" };
+            busFlow.AddActivity(activity3);
+            Activity activity4 = new Activity() { ActivityName = "Activity4" };
+            busFlow.AddActivity(activity4);
+            Activity activity5 = new Activity() { ActivityName = "Activity5" };
+            busFlow.AddActivity(activity5);
+            ActivitiesGroup group1 = new ActivitiesGroup() { Name = "Group1" };
+            busFlow.AddActivitiesGroup(group1);
+            ActivitiesGroup group2 = new ActivitiesGroup() { Name = "Group2" };
+            busFlow.AddActivitiesGroup(group2);
+            group1.AddActivityToGroup(activity1);
+            group2.AddActivityToGroup(activity4);
+
+            //Act
+            busFlow.AttachActivitiesGroupsAndActivities();
+
+            //Assert
+            Assert.IsTrue(busFlow.ActivitiesGroups.Count == 2, "Validate no extra groups were added");
+            Assert.IsTrue(group1.ActivitiesIdentifiers.Count == 3, "Validate first three Activities were attached to first group");
+            Assert.IsTrue(group2.ActivitiesIdentifiers.Count == 2, "Validate last two Activities were attached to second group");
+            Assert.IsTrue(group1.ActivitiesIdentifiers[2].ActivityName == activity3.ActivityName, "Validate group 1 has correct activities mapped to it");
+            Assert.IsTrue(group2.ActivitiesIdentifiers[1].ActivityName == activity5.ActivityName, "Validate group 2 has correct activities mapped to it");
+            Assert.IsTrue(activity2.ActivitiesGroupID == group1.Name, "Validate Activities mapped to group 1");
+            Assert.IsTrue(activity5.ActivitiesGroupID == group2.Name, "Validate Activities mapped to group 2");
+        }
+    }
+}
