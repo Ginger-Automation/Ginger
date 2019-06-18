026a2da08163e39a6f8dab3cdecb3edf10dd6e4c
Yaron Weiss <yaron.weiss@amdocs.com>
2019-06-18 16:22:38 -0500

    dummy change 14 check AppVeyor VS 2019

--- Ginger/GingerPluginCoreTest/CommunicationProtocol/WebPlatformServiceFakeLib/BrowserActionsFake.cs
+++ Ginger/GingerPluginCoreTest/CommunicationProtocol/WebPlatformServiceFakeLib/BrowserActionsFake.cs
@@ -12,7 +12,7 @@ namespace GingerPluginCoreTest.CommunicationProtocol.WebPlatformServiceFakeLib
 
         public void AcceptMessageBox()
         {
-            // dummy change 13 check AppVeyor VS 2019
+            // dummy change 14 check AppVeyor VS 2019
             throw new NotImplementedException();
         }
 
