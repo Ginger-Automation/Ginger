2224c72f8b33c79d6a35fdcb340cca383ae31b76
Yaron Weiss <yaron.weiss@amdocs.com>
2019-06-19 19:34:38 -0500

    dummy change 25 check AppVeyor VS 2019 back to 2017 with code needed

--- Ginger/GingerPluginCoreTest/CommunicationProtocol/GingerGridCommTest.cs
+++ Ginger/GingerPluginCoreTest/CommunicationProtocol/GingerGridCommTest.cs
@@ -1,110 +1,107 @@
-﻿//using amdocs.ginger.GingerCoreNET;
-//using Amdocs.Ginger.CoreNET.Drivers.CommunicationProtocol;
-//using Amdocs.Ginger.CoreNET.Run;
-//using Ginger.Plugin.Platform.Web;
-//using Ginger.Run;
-//using GingerCore;
-//using GingerCore.Actions;
-//using GingerCoreNET.Drivers.CommunicationProtocol;
-//using GingerCoreNET.DriversLib;
-//using GingerCoreNET.RunLib;
-//using GingerPluginCoreTest.CommunicationProtocol.WebPlatformServiceFakeLib;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Amdocs.Ginger.CoreNET.Drivers.CommunicationProtocol;
+using Amdocs.Ginger.CoreNET.Run;
+using Ginger.Plugin.Platform.Web;
+using GingerCore;
+using GingerCore.Actions;
+using GingerCoreNET.DriversLib;
+using GingerCoreNET.RunLib;
+using GingerPluginCoreTest.CommunicationProtocol.WebPlatformServiceFakeLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace GingerPluginCoreTest.CommunicationProtocol
-//{
+namespace GingerPluginCoreTest.CommunicationProtocol
+{
 
-//    // this tests are for checking plugin action pack to payload send via GingerNodeProxy, execute on GingerNode and service then getting response
+    // this tests are for checking plugin action pack to payload send via GingerNodeProxy, execute on GingerNode and service then getting response
 
-//    [TestClass]
-//    public class GingerGridCommTest
-//    {
-//        static GingerGrid gingerGrid;
-//        static GingerNode gingerNode;
-//        static IWebPlatform webPlatform;
-//        static GingerNodeProxy gingerNodeProxy;
-//        static Agent agent;
+    [TestClass]
+    public class GingerGridCommTest
+    {
+        static GingerGrid gingerGrid;
+        static GingerNode gingerNode;
+        static IWebPlatform webPlatform;
+        static GingerNodeProxy gingerNodeProxy;
+        static Agent agent;
 
-//        [ClassInitialize()]
-//        public static void ClassInit(TestContext context)
-//        {
-//            int port = SocketHelper.GetOpenPort();
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            int port = SocketHelper.GetOpenPort();
 
-//            // gingerGrid = WorkSpace.Instance.LocalGingerGrid; // new GingerGrid(port); 
-//            gingerGrid = new GingerGrid(port); 
-//            gingerGrid.Start();
+            // gingerGrid = WorkSpace.Instance.LocalGingerGrid; // new GingerGrid(port); 
+            gingerGrid = new GingerGrid(port);
+            gingerGrid.Start();
 
-//            // WorkSpace.Instance.LocalGingerGrid = gingerGrid;
+            // WorkSpace.Instance.LocalGingerGrid = gingerGrid;
 
-//            webPlatform = new WebPlatformServiceFake();
-//            gingerNode = new GingerNode(webPlatform);
-//            gingerNode.StartGingerNode("WebPlatformServiceFake 1", SocketHelper.GetLocalHostIP(), port);
+            webPlatform = new WebPlatformServiceFake();
+            gingerNode = new GingerNode(webPlatform);
+            gingerNode.StartGingerNode("WebPlatformServiceFake 1", SocketHelper.GetLocalHostIP(), port);
 
-//            // Wait for node to be connected.
+            // Wait for node to be connected.
 
-//            gingerNodeProxy = new GingerNodeProxy(gingerGrid.NodeList[0]);
-//            gingerNodeProxy.GingerGrid = gingerGrid;
+            gingerNodeProxy = new GingerNodeProxy(gingerGrid.NodeList[0]);
+            gingerNodeProxy.GingerGrid = gingerGrid;
 
-//            // GingerRunner gingerRunner = new GingerRunner();
-//            agent = new Agent();
-//            agent.GingerNodeProxy = gingerNodeProxy;
-//            agent.Platform = GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.PlatformsLib.ePlatformType.Service;
-//            // agent.PluginId = "aa";
-//            agent.ServiceId = "WebPlatformServiceFake";
-//            agent.AgentType = Agent.eAgentType.Service;
-//            agent.DriverConfiguration = new Amdocs.Ginger.Common.ObservableList<DriverConfigParam>();
+            // GingerRunner gingerRunner = new GingerRunner();
+            agent = new Agent();
+            agent.GingerNodeProxy = gingerNodeProxy;
+            agent.Platform = GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.PlatformsLib.ePlatformType.Service;
+            // agent.PluginId = "aa";
+            agent.ServiceId = "WebPlatformServiceFake";
+            agent.AgentType = Agent.eAgentType.Service;
+            agent.DriverConfiguration = new Amdocs.Ginger.Common.ObservableList<DriverConfigParam>();
 
-//            //agent.st
-//            // agent.StartDriver();
-//            gingerNodeProxy.StartDriver(agent.DriverConfiguration);
+            //agent.st
+            // agent.StartDriver();
+            gingerNodeProxy.StartDriver(agent.DriverConfiguration);
 
-//        }
+        }
 
 
-//        [ClassCleanup()]
-//        public static void ClassCleanup()
-//        {
-//            // gingerGrid.shutdown...
+        [ClassCleanup()]
+        public static void ClassCleanup()
+        {
+            // gingerGrid.shutdown...
 
 
 
-//        }
+        }
 
-//        [TestMethod]
-//        public void GotoURL2()
-//        {
-//            //Arrange
-//            string url = "aaa";
+        [TestMethod]
+        public void GotoURL2()
+        {
+            //Arrange
+            string url = "aaa";
 
-//            ActBrowserElement actBrowserElement = new ActBrowserElement();
-//            actBrowserElement.ControlAction = ActBrowserElement.eControlAction.GotoURL;
-//            // actBrowserElement.Value = url;   // Incorrect !!! need to use ValueForDriver
-//            actBrowserElement.ValueForDriver = url;
+            ActBrowserElement actBrowserElement = new ActBrowserElement();
+            actBrowserElement.ControlAction = ActBrowserElement.eControlAction.GotoURL;
+            // actBrowserElement.Value = url;   // Incorrect !!! need to use ValueForDriver
+            actBrowserElement.ValueForDriver = url;
 
-//            //Act            
-//            ExecuteOnPlugin.ExecutePlugInActionOnAgent(agent, actBrowserElement);
+            //Act            
+            ExecuteOnPlugin.ExecutePlugInActionOnAgent(agent, actBrowserElement);
 
-//            //Assert            
-//            Assert.IsTrue(string.IsNullOrEmpty(actBrowserElement.Error), "No Error");
+            //Assert            
+            Assert.IsTrue(string.IsNullOrEmpty(actBrowserElement.Error), "No Error");
 
-//            //FIXME !!!
-//            // Assert.IsTrue(string.IsNullOrEmpty(actBrowserElement.ExInfo), "Naviagted to: " + url, "ExInfo");
-//        }
+            //FIXME !!!
+            // Assert.IsTrue(string.IsNullOrEmpty(actBrowserElement.ExInfo), "Naviagted to: " + url, "ExInfo");
+        }
 
 
-//        [TestMethod]
-//        public void GotoURL()
-//        {
-//            // Arrange
-//            string url = "http://www.google.com";
+        [TestMethod]
+        public void GotoURL()
+        {
+            // Arrange
+            string url = "http://www.google.com";
 
-//            // Act
-//            webPlatform.BrowserActions.Navigate(url, "???");
-//            string browserurl = webPlatform.BrowserActions.GetCurrentUrl();
+            // Act
+            webPlatform.BrowserActions.Navigate(url, "???");
+            string browserurl = webPlatform.BrowserActions.GetCurrentUrl();
 
-//            //Assert
-//            Assert.AreEqual(url, browserurl, "URL of naviagte equel browser url");
+            //Assert
+            Assert.AreEqual(url, browserurl, "URL of naviagte equel browser url");
 
-//        }
-//    }
-//}
+        }
+    }
+}
