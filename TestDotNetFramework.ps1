d25bad467eef836c9e50e4baf3bc36d59705e023
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-01 11:20:30 -0500

    TestDotNetFramework.ps1 v1

---
+++ TestDotNetFramework.ps1
@@ -0,0 +1,2 @@
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\"
+./vstest.console.exe "d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll"
