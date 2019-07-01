2985d528cd4829f1f0bb558e2e30bf788aa56a9f
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-01 11:54:22 -0500

    Azure echo start end

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -1,2 +1,3 @@
+echo end
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\"
-vstest.console.exe "d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll"
+echo end
