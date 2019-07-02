a184e17c11e72c8718417b27105edf2089eabfeb
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-01 22:38:22 -0500

    ps script 2

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -1,3 +1,6 @@
-Write-Host "Hello World from $Env:AGENT_NAME."
-Write-Host "Hello World from $Env:AGENT_NAME."
+Write-Host "Starting test for .Net Framework DLLs"
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
+vstest.console.exe GingerCoreTest.dll d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx    
+
+Write-Host "Tests Completed"
 exit 0
