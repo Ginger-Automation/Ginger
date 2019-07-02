97a56e51336e0928b231dbe1f4e92e0113fab21a
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-02 10:31:39 -0500

    Azure ResultsDirectory

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -4,9 +4,11 @@ Write-Host "-------------------------------------------------------------"
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
 # cd "d:\a\1\s\Ginger\GingerCoreTest\bin\Release"
 # dir
-./vstest.console.exe d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll /Logger:trx 
+./vstest.console.exe d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll /Logger:trx /ResultsDirectory: d:\a\1\s\Ginger\ResultsDirectory
+cd d:\a\1\s\Ginger\ResultsDirectory
+dir
 # "vstest.console.exe GingerCoreTest.dll /Logger:trx"
-# ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx    
+# ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx   
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                        Tests Completed                    -"
 Write-Host "-------------------------------------------------------------"
