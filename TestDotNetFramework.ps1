bd18e2e7d5ca3c5266c797bb4d3fb5eaf98eb382
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-02 17:40:38 -0500

    Azure d:\a\1\s\TestResults

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -5,7 +5,7 @@ cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\E
 # cd "d:\a\1\s\Ginger\GingerCoreTest\bin\Release"
 # dir
 ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
-cd d:\a\1\s\Ginger\ResultsDirectory
+d:\a\1\s\TestResults
 dir
 # "vstest.console.exe GingerCoreTest.dll /Logger:trx"
 # ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx   
