4f3995e4f5bec6b3f65bb6c2984e82794fa9cf6c
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-02 11:52:55 -0500

    Azure result directory remove space

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -4,7 +4,7 @@ Write-Host "-------------------------------------------------------------"
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
 # cd "d:\a\1\s\Ginger\GingerCoreTest\bin\Release"
 # dir
-./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx /ResultsDirectory: d:\a\1\s\TestResults
+./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
 cd d:\a\1\s\Ginger\ResultsDirectory
 dir
 # "vstest.console.exe GingerCoreTest.dll /Logger:trx"
