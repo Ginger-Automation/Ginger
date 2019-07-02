dbbb58735bfb5669579f5b7cc652acce0d672c81
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-01 22:54:52 -0500

    ps1 with unit tests

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -1,7 +1,11 @@
-Write-Host "Starting test for .Net Framework DLLs"
+Write-Host "-------------------------------------------------------------"
+Write-Host "-          Starting test for .Net Framework DLLs            -"
+Write-Host "-------------------------------------------------------------"
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
 dir
 ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx    
-
-Write-Host "Tests Completed"
+./vstest.console.exe d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll /Logger:trx    
+Write-Host "-------------------------------------------------------------"
+Write-Host "-                        Tests Completed                    -"
+Write-Host "-------------------------------------------------------------"
 exit 0
