e66546bbbc691838e13fb7f141ce40577b3596b7
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-02 10:27:53 -0500

    Azure GingerCoreTest only

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -1,11 +1,12 @@
 Write-Host "-------------------------------------------------------------"
 Write-Host "-          Starting test for .Net Framework DLLs            -"
 Write-Host "-------------------------------------------------------------"
-# cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
-cd "d:\a\1\s\Ginger\GingerCoreTest\bin\Release"
-dir
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\vstest.console.exe GingerCoreTest.dll /Logger:trx"
-# ./vstest.console.exe d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll /Logger:trx    
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
+# cd "d:\a\1\s\Ginger\GingerCoreTest\bin\Release"
+# dir
+./vstest.console.exe d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll /Logger:trx 
+# "vstest.console.exe GingerCoreTest.dll /Logger:trx"
+# ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx    
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                        Tests Completed                    -"
 Write-Host "-------------------------------------------------------------"
