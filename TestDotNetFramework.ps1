7ed4dc9416f6dea74c8a1a3f3db3bf14ef6a75a3
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-03 08:19:30 -0500

    Unit Tests 2

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -6,7 +6,7 @@ cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\E
 # dir
 ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
 ./vstest.console.exe d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
-./vstest.console.exe d:\a\1\s\Ginger\GingerTest\bin\Release\GingerTest.dll /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
+./vstest.console.exe "d:\a\1\s\Ginger\GingerTest\bin\Release\GingerTest.dll" /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
 cd d:\a\1\s\TestResults
 dir
 # "vstest.console.exe GingerCoreTest.dll /Logger:trx"
