8d07e163493095629cbb2868387c5daa9aa36c46
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-03 09:55:41 -0500

    Azure last exit code

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -2,15 +2,34 @@ Write-Host "-------------------------------------------------------------"
 Write-Host "-          Starting test for .Net Framework DLLs            -"
 Write-Host "-------------------------------------------------------------"
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
-# cd "d:\a\1\s\Ginger\GingerCoreTest\bin\Release"
-# dir
+
+$exitcode=0
+
 ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
+Write-Host ">>>>>>>>>>>>>>>>>>>>>>>>>>>>> LastExitCode: " + $LastExitCode
+if ($LastExitCode != 0)
+{
+	$exitcode = 1
+}
+
 ./vstest.console.exe "d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll" /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
+Write-Host ">>>>>>>>>>>>>>>>>>>>>>>>>>>>> LastExitCode: " + $LastExitCode
+if ($LastExitCode != 0)
+{
+	$exitcode = 1
+}
+
 ./vstest.console.exe "d:\a\1\s\Ginger\GingerTest\bin\Release\GingerTest.dll" /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
+Write-Host ">>>>>>>>>>>>>>>>>>>>>>>>>>>>> LastExitCode: " + $LastExitCode
+if ($LastExitCode != 0)
+{
+	$exitcode = 1
+}
+
 cd d:\a\1\s\TestResults
 dir
-# "vstest.console.exe GingerCoreTest.dll /Logger:trx"
-# ./vstest.console.exe d:\a\1\s\Ginger\GingerCoreTest\bin\Release\GingerCoreTest.dll /Logger:trx   
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                        Tests Completed                    -"
 Write-Host "-------------------------------------------------------------"
+
+exit $exitcode
