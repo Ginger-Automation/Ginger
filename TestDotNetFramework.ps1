3b2433f9137c9a18c16110177a1d91b146431bd2
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-03 10:37:01 -0500

    Azure ps1 UnitTests commented

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -1,6 +1,7 @@
 Write-Host "-------------------------------------------------------------"
 Write-Host "-          Starting test for .Net Framework DLLs            -"
 Write-Host "-------------------------------------------------------------"
+
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\TestPlatform\" 
 
 $exitcode=0
@@ -12,7 +13,7 @@ if ($LastExitCode -ne 0)
 	$exitcode = 1
 }
 
-./vstest.console.exe "d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll" /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
+# ./vstest.console.exe "d:\a\1\s\Ginger\Unit Tests\bin\Release\UnitTests.dll" /Logger:trx /ResultsDirectory:d:\a\1\s\TestResults
 Write-Host ">>>>>>>>>>>>>>>>>>>>>>>>>>>>> LastExitCode: " $LastExitCode
 if ($LastExitCode -ne 0)
 {
