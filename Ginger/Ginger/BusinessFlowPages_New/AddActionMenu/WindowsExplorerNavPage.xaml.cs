e5d68575d1413b0664739d12eb7666294e35f53c
shahanemahesh <mahesh.shahane@amdocs.com>
2019-07-09 21:51:02 +0530

    Updated the usings

--- Ginger/Ginger/BusinessFlowPages_New/AddActionMenu/WindowsExplorerNavPage.xaml.cs
+++ Ginger/Ginger/BusinessFlowPages_New/AddActionMenu/WindowsExplorerNavPage.xaml.cs
@@ -1,31 +1,12 @@
 ﻿using Amdocs.Ginger.Common;
 using Amdocs.Ginger.Common.UIElement;
-using Amdocs.Ginger.Repository;
 using Ginger.BusinessFlowPages_New;
-using Ginger.Drivers.Common;
 using Ginger.WindowExplorer;
-using Ginger.WindowExplorer.Common;
-using GingerCore;
-using GingerCore.Actions;
 using GingerCore.Platforms;
-using GingerCore.Platforms.PlatformsInfo;
 using GingerCoreNET;
-using GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.PlatformsLib;
-using GingerWPF.UserControlsLib.UCTreeView;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Ginger.BusinessFlowsLibNew.AddActionMenu
 {
