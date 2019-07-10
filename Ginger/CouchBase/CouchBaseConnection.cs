fbd32f483a10f45bb7b94a20dd6232758957c1e7
Kashmira <Kashmira.Deshpande@amdocs.com>
2019-07-10 17:45:01 +0530

    Couchbase +PostgreSQL

---
+++ Ginger/CouchBase/CouchBaseConnection.cs
@@ -0,0 +1,153 @@
+﻿using Amdocs.Ginger.Common;
+using Couchbase;
+using Couchbase.Authentication;
+using Couchbase.Configuration.Client;
+using Couchbase.N1QL;
+using GingerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CouchBase
+{
+    public class CouchBaseConnection : Amdocs.Ginger.CoreNET.IDatabase
+    {
+        Cluster clusterCB = null;
+        public Dictionary<string, string> KeyvalParamatersList = new Dictionary<string, string>();
+        string ConnectionString = null;
+        string User = null;
+        string Password = null;
+        string TNS = null;
+        IQueryResult<dynamic> result = null;
+        string bucketName = null;
+
+        public void GetConnectionString(Dictionary<string, string> parameters)
+        {
+            ConnectionString = parameters.FirstOrDefault(pair => pair.Key == "ConnectionString").Value;
+            User = parameters.FirstOrDefault(pair => pair.Key == "UserName").Value;
+            Password = parameters.FirstOrDefault(pair => pair.Key == "Password").Value;
+            TNS = parameters.FirstOrDefault(pair => pair.Key == "TNS").Value;
+        }
+        public bool OpenConnection(Dictionary<string, string> parameters)
+        {
+            KeyvalParamatersList = parameters;
+            GetConnectionString(parameters);
+            try
+            {
+                clusterCB = new Couchbase.Cluster(new ClientConfiguration
+                {
+                    ViewRequestTimeout = 45000,
+                    Servers = new List<Uri> { new Uri(TNS) },
+                });
+                bool res = false;
+                //TODO: need to decrypt the password in the Database->PassCalculated
+                String deCryptValue = EncryptionHandler.DecryptString(Password, ref res, false);
+                if (res == true)
+                {
+                    clusterCB.Authenticate(User, deCryptValue);
+                }
+                else
+                {
+                    clusterCB.Authenticate(User, Password);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Reporter.ToLog(eLogLevel.ERROR, "Failed to connect to Couchbase DB", e);
+                return false;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            clusterCB.Dispose();
+        }
+
+        public List<object> DBQuery(string Query)
+        {
+            List<object> list = new List<object>();
+            DoBeforeExecutionOperations(Query);
+            result = clusterCB.Query<dynamic>(Query);
+            for (int i = 0; i < result.Rows.Count; i++)
+            {
+                list.Add(result.Rows[i]);
+                //Act.ParseJSONToOutputValues(result.Rows[i].ToString(), i + 1);
+            }
+            return list;
+        }
+        private string GetBucketName(string inputSQL, bool IftinputSQL= false)
+        {
+            string bucketName = string.Empty;
+            
+            if (IftinputSQL)
+            {
+                bucketName = inputSQL.Replace("`", "");
+                bucketName = bucketName.Replace("'", "");
+            }
+            else
+            {
+                bucketName = inputSQL.Substring(inputSQL.IndexOf(" from ") + 6);
+                bucketName = bucketName.Substring(0, bucketName.IndexOf(" ")).Replace("`", "");
+            }
+            return bucketName;
+        }
+        public bool ConnecttoBucket(string bucketName)
+        {
+            try
+            {
+                var bucket = clusterCB.OpenBucket(bucketName);
+                string bucketpassword = bucket.Configuration.Password;
+                ClassicAuthenticator classicAuthenticator = new ClassicAuthenticator(User, Password);
+                classicAuthenticator.AddBucketCredential(bucketName, bucketpassword);
+                clusterCB.Authenticate(classicAuthenticator);
+                //TODO: need to check and true and flase on basis of connection
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Reporter.ToLog(eLogLevel.ERROR, "Failed To Connect ConnectToBucket Method In GingerCouchBase DB", ex);
+                return false;
+            }
+        }
+        public void DoBeforeExecutionOperations(string SQL, bool ifTbalename= false)
+        {
+            if (!OpenConnection(KeyvalParamatersList))
+            {
+                return;
+            }
+            bucketName = GetBucketName(SQL, ifTbalename);
+
+            if (!ConnecttoBucket(bucketName))
+            {
+                return;
+            }
+        }
+        public int GetRecordCount(string Query)
+        {
+            DoBeforeExecutionOperations(Query, true);
+            result = clusterCB.Query<dynamic>("Select Count(*) as RECORDCOUNT from `" + bucketName + "`");
+            return int.Parse(result.Rows[0].ToString());
+        }
+
+        public string GetSingleValue(string Table, string Column, string Where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetTablesColumns(string table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetTablesList(string Name = null)
+        {
+            throw new NotImplementedException();
+        }
+        public string RunUpdateCommand(string updateCmd, bool commit = true)
+        {
+            var RS1 = clusterCB.Query<dynamic>(updateCmd);
+            return RS1.ToString();
+        }
+    }
+}
---
+++ Ginger/PostgreSQL/PostgreSQLConnection.cs
@@ -0,0 +1,117 @@
+﻿using Amdocs.Ginger.Common;
+using GingerCore;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+
+namespace PostgreSQL
+{
+    public class PostgreSQLConnection : Amdocs.Ginger.CoreNET.IDatabase
+    {
+        private DbConnection oConn = null;
+        private DbTransaction tran = null;
+        public Dictionary<string, string> KeyvalParamatersList = new Dictionary<string, string>();
+        public string GetConnectionString(Dictionary<string, string> parameters)
+        {
+            string connStr = null;
+            bool res;
+            res = false;
+            string ConnectionString = parameters.FirstOrDefault(pair => pair.Key == "ConnectionString").Value;
+            string User = parameters.FirstOrDefault(pair => pair.Key == "UserName").Value;
+            string Password = parameters.FirstOrDefault(pair => pair.Key == "Password").Value;
+            string TNS = parameters.FirstOrDefault(pair => pair.Key == "TNS").Value;
+            string Name = parameters.FirstOrDefault(pair => pair.Key == "Name").Value;
+
+            if (String.IsNullOrEmpty(ConnectionString) == false)
+            {
+                connStr = ConnectionString.Replace("{USER}", User);
+
+                String deCryptValue = EncryptionHandler.DecryptString(Password, ref res, false);
+                if (res == true)
+                { connStr = connStr.Replace("{PASS}", deCryptValue); }
+                else
+                { connStr = connStr.Replace("{PASS}", Password); }
+            }
+            else
+            {
+                String strConnString = TNS;
+               
+                connStr = "Data Source=" + TNS + ";User Id=" + User + ";";
+
+                String deCryptValue = EncryptionHandler.DecryptString(Password, ref res, false);
+                string[] host = TNS.Split(':');
+                if (host.Length == 2)
+                {
+                    connStr = String.Format("Server ={0};Port={1};User Id={2}; Password={3};Database={4};", host[0], host[1], User, deCryptValue, Name);
+                }
+                else
+                {
+                    connStr = String.Format("Server ={0};User Id={1}; Password={2};Database={3};", TNS, User, deCryptValue, Name);
+                }
+            }
+
+            return connStr;
+        }
+        public bool OpenConnection(Dictionary<string, string> parameters)
+        {
+            KeyvalParamatersList = parameters;
+            string connectConnectionString = GetConnectionString(parameters);
+            oConn = new NpgsqlConnection(connectConnectionString);
+            oConn.Open();
+            return true;
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                if (oConn != null)
+                {
+                    oConn.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Reporter.ToLog(eLogLevel.ERROR, "Failed to close DB Connection", e);
+                throw (e);
+            }
+            finally
+            {
+                oConn?.Dispose();
+            }
+        }
+
+        public List<object> DBQuery(string Query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRecordCount(string Query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSingleValue(string Table, string Column, string Where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetTablesColumns(string table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetTablesList(string Name = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public string RunUpdateCommand(string updateCmd, bool commit = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
