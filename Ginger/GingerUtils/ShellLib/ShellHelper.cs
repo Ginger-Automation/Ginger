a17a12e9476f7b5a3fbaa78b9efe4520a14ca2b7
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-11 15:34:39 -0500

    ShellHelper with dotnet

--- Ginger/GingerUtils/ShellLib/ShellHelper.cs
+++ Ginger/GingerUtils/ShellLib/ShellHelper.cs
@@ -2,6 +2,52 @@
 using System.Diagnostics;
 public static class ShellHelper
 {
+
+    /// <summary>
+    /// Shell and create new process which runs dotent and the args
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Dotnet(string args)
+    {
+        System.Diagnostics.ProcessStartInfo procStartInfo = null;
+        if (GingerUtils.OperatingSystem.IsWindows())
+        {
+            Console.WriteLine("*** OS is Windows ***");
+            procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + args);
+            procStartInfo.UseShellExecute = true;
+        }
+        else if (GingerUtils.OperatingSystem.IsLinux())
+        {
+            Console.WriteLine("*** OS is Linux ***");
+            var output = ShellHelper.Bash(args);
+            Console.WriteLine("=====================================================================");
+            Console.WriteLine("<<<<<<<<<<<<<<<          Shell result                    >>>>>>>>>>>>");
+            
+            Console.WriteLine(output);
+            Console.WriteLine("=====================================================================");
+
+
+            // cmd = "-c \"gnome-terminal -x bash -ic 'cd $HOME; dotnet " + dll + " " + nodeFileName + "'\"";  // work for Redhat
+            string cmd = args = "-c \"'dotnet " + args;  
+            Console.WriteLine("Command: " + args);
+            procStartInfo = new System.Diagnostics.ProcessStartInfo();
+            procStartInfo.FileName = "/bin/bash";
+            procStartInfo.Arguments = args;
+
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = false;
+            procStartInfo.RedirectStandardOutput = true;
+        }
+
+        System.Diagnostics.Process proc = new System.Diagnostics.Process();
+        proc.StartInfo = procStartInfo;
+
+        Console.WriteLine("Starting Process..");
+        proc.Start();
+    }
+
+
+
     public static string Bash(string cmd)
     {
         var escapedArgs = cmd.Replace("\"", "\\\"");
