16a3a42758fc39fcadb8033311ccc81321de5549
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-26 14:01:32 -0500

    GingerUtils with GingerTestHelper and new StringCompressorTest

---
+++ Ginger/GingerUtilsTest/StringCompressorTest.cs
@@ -0,0 +1,56 @@
+﻿using GingerTestHelper;
+using GingerUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GingerUtilsTest
+{
+    [TestClass]
+    public class StringCompressorTest
+    {
+        static TestHelper mTestHelper = new TestHelper();
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext TestContext)
+        {
+            mTestHelper.ClassInitialize(TestContext);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            mTestHelper.ClassCleanup();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mTestHelper.TestInitialize(TestContext);
+        }
+
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            mTestHelper.TestCleanup();
+        }
+
+        [TestMethod]
+        public void SampleTest1()
+        {
+            //Arrange
+            string s1 = "abcdefghijklmnop";
+
+
+            //Act
+            string compressed = StringCompressor.CompressString(s1);
+            string uncompressed = StringCompressor.DecompressString(compressed);
+
+            //Assert
+            Assert.AreEqual(uncompressed, s1, "uncompressed=s1");
+        }
+    }
+}
--- Ginger/GingerUtilsTest/TestAssemblyInit.cs
+++ Ginger/GingerUtilsTest/TestAssemblyInit.cs
@@ -1,9 +1,8 @@
-﻿using Amdocs.Ginger.Common;
-using GingerTestHelper;
+﻿using GingerTestHelper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Reflection;
 
-namespace GingerCoreNETUnitTest
+namespace GingerUtilsTest
 {
     [TestClass]
     public class TestAssemblyInit
@@ -14,9 +13,7 @@ namespace GingerCoreNETUnitTest
             // Called once when the test assembly is loaded
             // We provide the assembly to GingerTestHelper.TestResources so it can locate the 'TestResources' folder path
             TestResources.Assembly = Assembly.GetExecutingAssembly();
-
-            // Init Reporter
-            Reporter.WorkSpaceReporter = new UnitTestWorkspaceReporter();
+            
         }
     }
 }
