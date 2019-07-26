306bf7016b282fa1274a285003094272461d890b
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-26 15:37:49 -0500

    Azure windows copy all Artifacts of all projects

--- TestDotNetFramework.ps1
+++ TestDotNetFramework.ps1
@@ -35,6 +35,14 @@ Write-Host "-------------------------------------------------------------"
 New-Item -Path "d:\a\1\a" -Name "TestCompleted.txt" -ItemType "file" -Value "Test Completed Artifacts"
 
 Compress-Archive -Path 'd:\a\1\s\Ginger\GingerUtilsTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerUtilsTestArtifacts'
+Compress-Archive -Path 'd:\a\1\s\Ginger\GingerCoreCommonTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerCoreCommonTestArtifacts'
+Compress-Archive -Path 'd:\a\1\s\Ginger\GingerCoreNETUnitTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerCoreNETUnitTestArtifacts'
+Compress-Archive -Path 'd:\a\1\s\Ginger\GingerConsoleTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerConsoleTestArtifacts'
+Compress-Archive -Path 'd:\a\1\s\Ginger\GingerAutoPilotTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerAutoPilotTestArtifacts'
+Compress-Archive -Path 'd:\a\1\s\Ginger\GingerPluginCoreTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerPluginCoreTestArtifacts'
+      
+Compress-Archive -Path 'd:\a\1\s\Ginger\GingerCoreTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerCoreTestTestArtifacts'
+Compress-Archive -Path 'd:\a\1\s\Ginger\GingerTest\bin\Release\netcoreapp2.2\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerTestArtifacts'
 
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                         Artifacts List                    -"
