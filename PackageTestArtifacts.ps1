dce2a724b11b99b5cbd16bc2206a1a06be101a23
Yaron Weiss <yaron.weiss@amdocs.com>
2019-07-27 08:47:28 -0500

    Azure Linux removed zip

--- PackageTestArtifacts.ps1
+++ PackageTestArtifacts.ps1
@@ -3,9 +3,9 @@ Write-Host "-                    Package Test Artifacts                 -"
 Write-Host "-------------------------------------------------------------"
 New-Item -Path "/home/vsts/work/1/a" -Name "TestCompleted.txt" -ItemType "file" -Value "Test Completed Artifacts"
 
-Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerUtilsTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerUtilsTestArtifacts'
-Compress-Archive -Path '/home/vsts/work/1/s/GingerCoreCommonTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerCoreCommonTestArtifacts'
-Compress-Archive -Path '/home/vsts/work/1/s/GingerCoreNETUnitTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerCoreNETUnitTestArtifacts'
+# Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerUtilsTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerUtilsTestArtifacts'
+# Compress-Archive -Path '/home/vsts/work/1/s/GingerCoreCommonTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerCoreCommonTestArtifacts'
+# Compress-Archive -Path '/home/vsts/work/1/s/GingerCoreNETUnitTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerCoreNETUnitTestArtifacts'
 # Compress-Archive -Path '/home/vsts/work/1/s/GingerConsoleTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerConsoleTestArtifacts'
 # Compress-Archive -Path '/home/vsts/work/1/s/GingerAutoPilotTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerAutoPilotTestArtifacts'
 # Compress-Archive -Path '/home/vsts/work/1/s/GingerPluginCoreTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerPluginCoreTestArtifacts'
