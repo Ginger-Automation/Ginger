0746cb33bc6bd6d7d6b2e7af0633e8c27c57e42c
Mohd Ambar Khan <MohdAmbar.Khan@amdocs.com>
2019-07-30 16:18:12 +0530

    implementing interface for fake browser action

--- Ginger/GingerPluginCoreTest/CommunicationProtocol/WebPlatformServiceFakeLib/BrowserActionsFake.cs
+++ Ginger/GingerPluginCoreTest/CommunicationProtocol/WebPlatformServiceFakeLib/BrowserActionsFake.cs
@@ -28,6 +28,11 @@ namespace GingerPluginCoreTest.CommunicationProtocol.WebPlatformServiceFakeLib
 
         string mURL;
 
+        public void AcceptAlert()
+        {
+            throw new NotImplementedException();
+        }
+
         public void AcceptMessageBox()
         {
             // dummy change 25 check AppVeyor VS 2019
@@ -49,6 +54,11 @@ namespace GingerPluginCoreTest.CommunicationProtocol.WebPlatformServiceFakeLib
             throw new NotImplementedException();
         }
 
+        public void DismissAlert()
+        {
+            throw new NotImplementedException();
+        }
+
         public void DismissMessageBox()
         {
             throw new NotImplementedException();
@@ -64,9 +74,19 @@ namespace GingerPluginCoreTest.CommunicationProtocol.WebPlatformServiceFakeLib
             throw new NotImplementedException();
         }
 
+        public string GetAlertText()
+        {
+            throw new NotImplementedException();
+        }
+
         public string GetCurrentUrl()
         {
             return mURL;
+        }
+
+        public string GetPageSource()
+        {
+            throw new NotImplementedException();
         }
 
         public string GetTitle()
@@ -110,6 +130,11 @@ namespace GingerPluginCoreTest.CommunicationProtocol.WebPlatformServiceFakeLib
         }
 
         public void Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendAlertText(string Text)
         {
             throw new NotImplementedException();
         }
