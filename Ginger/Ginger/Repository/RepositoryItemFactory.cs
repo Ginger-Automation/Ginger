85dcf2452cfd49e94a068f39bf6f2a23b2a2134b
Kashmira <Kashmira.Deshpande@amdocs.com>
2019-07-31 15:22:24 +0530

    After review changes

--- Ginger/Ginger/Repository/RepositoryItemFactory.cs
+++ Ginger/Ginger/Repository/RepositoryItemFactory.cs
@@ -560,10 +560,11 @@ namespace Ginger.Repository
         {
             HTMLReportConfiguration currentTemplate = (HTMLReportConfiguration)a;
             System.Drawing.Image CustomerLogo = Ginger.General.Base64StringToImage(currentTemplate.LogoBase64Image.ToString());
-            if (Directory.Exists(tempFolder))
+            if (!Directory.Exists(tempFolder))
             {
-                CustomerLogo.Save(tempFolder + "/CustomerLogo.png");
+                Directory.CreateDirectory(tempFolder); 
             }
+            CustomerLogo.Save(tempFolder + "/CustomerLogo.png");
             Ginger.Reports.HTMLReportTemplatePage.EnchancingLoadedFieldsWithDataAndValidating(currentTemplate);
         }
 
