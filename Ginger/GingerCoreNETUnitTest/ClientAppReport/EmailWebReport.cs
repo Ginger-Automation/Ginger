b0cc5ea7fee453a412e63ec74a5e7339f46c9b00
Yaron Weiss <yaron.weiss@amdocs.com>
2019-08-16 11:13:33 -0500

    EmailWebReport comments and remove ignore

--- Ginger/GingerCoreNETUnitTest/ClientAppReport/EmailWebReport.cs
+++ Ginger/GingerCoreNETUnitTest/ClientAppReport/EmailWebReport.cs
@@ -1,5 +1,6 @@
 ﻿using amdocs.ginger.GingerCoreNET;
 using Amdocs.Ginger.Common;
+using Amdocs.Ginger.CoreNET.Logger;
 using Amdocs.Ginger.CoreNET.Reports.ReportHelper;
 using Amdocs.Ginger.CoreNET.RunLib;
 using Amdocs.Ginger.CoreNET.RunLib.CLILib;
@@ -9,40 +10,52 @@ using GingerCore.Environments;
 using GingerCoreNETUnitTest.RunTestslib;
 using GingerTestHelper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace GingerCoreNETUnitTest.ClientAppReport
 {
     [TestClass]
+
+    // Change class name to EmailWebReportTest
     public class EmailWebReport
     {
         #region Data Members
         static TestHelper mTestHelper = new TestHelper();
         public TestContext TestContext { get; set; }
 
-        private string mTempFolder;
-        private string mSolutionFolder;
+        static string mTempFolder;
+        static string mSolutionFolder;
         #endregion
 
-        #region Ctor
-        public EmailWebReport()
-        {
-            mTempFolder = TestResources.GetTempFolder("CLI Tests");
-            mSolutionFolder = Path.Combine(TestResources.GetTestResourcesFolder(@"Solutions"), "EmailWebReport");
-            Reporter.WorkSpaceReporter = new UnitTestWorkspaceReporter();
-            RunFlow();
-        }
-        #endregion
 
         #region Events
         [ClassInitialize()]
         public static void ClassInit(TestContext TestContext)
         {
             mTestHelper.ClassInitialize(TestContext);
+
+            mTempFolder = TestResources.GetTempFolder("CLI Tests");
+            mSolutionFolder = Path.Combine(TestResources.GetTestResourcesFolder(@"Solutions"), "EmailWebReport");
+            Reporter.WorkSpaceReporter = new UnitTestWorkspaceReporter();
+
+            CreateWorkspace();
+        }
+
+        static void CreateWorkspace()
+        {
+            WorkSpaceEventHandler WSEH = new WorkSpaceEventHandler();
+            WorkSpace.Init(WSEH);
+            WorkSpace.Instance.RunningFromUnitTest = true;
+            WorkSpace.Instance.InitWorkspace(new GingerUnitTestWorkspaceReporter(), new RepoCoreItem());
+            WorkSpace.Instance.OpenSolution(mSolutionFolder);
+            WorkSpace.Instance.Solution.LoggerConfigurations.SelectedDataRepositoryMethod = Ginger.Reports.ExecutionLoggerConfiguration.DataRepositoryMethod.LiteDB;
+            SolutionRepository SR = WorkSpace.Instance.SolutionRepository;
+            RunsetExecutor runsetExecutor = new RunsetExecutor();
+            runsetExecutor.RunsetExecutionEnvironment = (from x in SR.GetAllRepositoryItems<ProjEnvironment>() where x.Name == "Default" select x).SingleOrDefault();
+            runsetExecutor.RunSetConfig = (from x in SR.GetAllRepositoryItems<RunSetConfig>() where x.Name == "Default Run Set" select x).SingleOrDefault();
+            WorkSpace.Instance.RunsetExecutor = runsetExecutor;
+            WorkSpace.Instance.RunsetExecutor.InitRunners();
         }
 
         [ClassCleanup]
@@ -64,43 +77,11 @@ namespace GingerCoreNETUnitTest.ClientAppReport
         }
         #endregion
 
-        [Ignore]   // fail on Azure
+        // [Ignore]   // fail on Azure - TODO: remove ignore and check why it fails
         [TestMethod]
-        public void TestNewReportExecData()
+        public void CLIRunSetWithSendEmailReport()
         {
-            string clientAppFilePath = Path.Combine(WorkSpace.Instance.LocalUserApplicationDataFolderPath, "Reports", "Ginger-Web-Client", "assets", "Execution_Data", "executiondata.js");
-            bool isFileExists = File.Exists(clientAppFilePath);
-            string jsDataStr = string.Empty;
-            if (isFileExists)
-                jsDataStr = File.ReadAllText(clientAppFilePath);
-            Assert.IsTrue(isFileExists && jsDataStr.StartsWith("window.runsetData={\"GingerVersion\""));
-        }
-
-        [TestMethod]
-        public void TestNewReportFolderCreation()
-        {
-            string clientAppFolderPath = Path.Combine(WorkSpace.Instance.LocalUserApplicationDataFolderPath, "Reports", "Ginger-Web-Client");
-            Assert.IsTrue(Directory.Exists(clientAppFolderPath));
-        }
-
-        private void CreateWorkspace()
-        {
-            WorkSpaceEventHandler WSEH = new WorkSpaceEventHandler();
-            WorkSpace.Init(WSEH);
-            WorkSpace.Instance.RunningFromUnitTest = true;
-            WorkSpace.Instance.InitWorkspace(new GingerUnitTestWorkspaceReporter(), new RepoCoreItem());
-            WorkSpace.Instance.OpenSolution(mSolutionFolder);
-            WorkSpace.Instance.Solution.LoggerConfigurations.SelectedDataRepositoryMethod = Ginger.Reports.ExecutionLoggerConfiguration.DataRepositoryMethod.LiteDB;
-            SolutionRepository SR = WorkSpace.Instance.SolutionRepository;
-            RunsetExecutor runsetExecutor = new RunsetExecutor();
-            runsetExecutor.RunsetExecutionEnvironment = (from x in SR.GetAllRepositoryItems<ProjEnvironment>() where x.Name == "Default" select x).SingleOrDefault();
-            runsetExecutor.RunSetConfig = (from x in SR.GetAllRepositoryItems<RunSetConfig>() where x.Name == "Default Run Set" select x).SingleOrDefault();
-            WorkSpace.Instance.RunsetExecutor = runsetExecutor;
-            WorkSpace.Instance.RunsetExecutor.InitRunners();
-        }
-        private void RunFlow()
-        {
-            CreateWorkspace();
+            //Arrange
             // Create config file
             CLIHelper cLIHelper = new CLIHelper();
             cLIHelper.RunAnalyzer = true;
@@ -109,10 +90,73 @@ namespace GingerCoreNETUnitTest.ClientAppReport
             RunSetAutoRunConfiguration runSetAutoRunConfiguration = new RunSetAutoRunConfiguration(WorkSpace.Instance.Solution, WorkSpace.Instance.RunsetExecutor, cLIHelper);
             runSetAutoRunConfiguration.ConfigFileFolderPath = mTempFolder;
             runSetAutoRunConfiguration.SelectedCLI = new CLIArgs();
-            // Act            
             CLIProcessor CLI = new CLIProcessor();
+
+            // Act
             CLI.ExecuteArgs(new string[] { runSetAutoRunConfiguration.SelectedCLI.Identifier + "=" + runSetAutoRunConfiguration.ConfigFileContent });
+            string clientAppFilePath = Path.Combine(WorkSpace.Instance.LocalUserApplicationDataFolderPath, "Reports", "Ginger-Web-Client", "assets", "Execution_Data", "executiondata.js");
+            bool isFileExists = File.Exists(clientAppFilePath);
+            string jsDataStr = File.ReadAllText(clientAppFilePath);
+
+            //Artifacts
+            // TODO: zip and upload the report - create it in temp folder
+            // mTestHelper.AddTestArtifact("...\Report.zip" - zipFileName);
+
+            //Assert
+            Assert.IsTrue(jsDataStr.StartsWith("window.runsetData={\"GingerVersion\""), "jsDataStr.StartsWith(window.runsetData ={\"GingerVersion\"");
+            Assert.AreEqual(true, isFileExists, "clientAppFilePath exist - " + clientAppFilePath);
+
+            // TODO: verify the content of the jsDataStr
+            //Assert.AreEqual(....
         }
 
+        [TestMethod]
+        public void TestNewReportFolderCreation()
+        {
+            // Arrange
+
+
+            //Act
+
+            // Which method are we testing here Path.Combine ??? the creation of the folder?
+            string clientAppFolderPath = Path.Combine(WorkSpace.Instance.LocalUserApplicationDataFolderPath, "Reports", "Ginger-Web-Client");
+
+            //Assert
+            Assert.IsTrue(Directory.Exists(clientAppFolderPath));
+        }
+
+        [TestMethod]
+        public void CopyWebRep()
+        {
+            //Arrange
+            WebReportGenerator webReportGenerator = new WebReportGenerator();
+            // webReportGenerator.  - check method of WebReportGenerator - no need to run run set, just use LiteDB samples or json data file
+
+            //Act
+
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void VerifyReportHTML()
+        {
+            //Arrange
+            // Start Chrome driver using selenium
+
+            //Act
+            // Got report url
+
+            // Verify html elements of UI
+            //Assert
+
+        }
+
+
+
+
     }
+
+
+
 }
