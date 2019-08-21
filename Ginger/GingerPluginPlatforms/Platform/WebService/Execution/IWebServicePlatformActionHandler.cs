25b10422b67f93f91e3a06314bf8028fa9b8bac9
Ambar <ambar_khan@live.com>
2019-08-21 18:16:12 +0530

    Initial Preparation for WebServicePlugin

---
+++ Ginger/GingerPluginPlatforms/Platform/WebService/Execution/IWebServicePlatformActionHandler.cs
@@ -0,0 +1,16 @@
+﻿using Amdocs.Ginger.CoreNET.RunLib;
+using Amdocs.Ginger.Plugin.Core.ActionsLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ginger.Plugin.Platform.WebService.Execution
+{
+    public class WebServicePlatformActionHandler : IPlatformActionHandler
+    {
+        public void HandleRunAction(IPlatformService service, ref NodePlatformAction platformAction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
