06e45a4d4cfb889aef20d8ee6cf1255fd5e38d76
shahanemahesh <mahesh.shahane@amdocs.com>
2019-08-29 14:41:25 +0530

    Updated code in conversion class

--- Ginger/Ginger/Actions/ApiActionsConversion/ApiActionConversionUtils.cs
+++ Ginger/Ginger/Actions/ApiActionsConversion/ApiActionConversionUtils.cs
@@ -16,6 +16,8 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib.ActionsConversion
     /// </summary>
     public class ApiActionConversionUtils
     {
+        private string WEB_SERVICE = "WebServices";
+
         /// <summary>
         /// This method is used to convert the legacy service actions to api actions from the businessflows provided
         /// </summary>
@@ -33,49 +35,32 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib.ActionsConversion
                         for (int intIndex = 0; intIndex < bf.BusinessFlow.Activities.Count(); intIndex++)
                         {
                             Activity activity = bf.BusinessFlow.Activities[intIndex];
-                            if (activity != null && activity.Active && (activity.TargetApplication.StartsWith("WebServices") || activity.TargetApplication.Contains("WebServices")))
+                            if (activity != null && activity.Active && (activity.TargetApplication.StartsWith(WEB_SERVICE) || activity.TargetApplication.Contains(WEB_SERVICE)))
                             {
-                                Activity currentActivity;
-                                currentActivity = new Activity() { Active = true };
-                                currentActivity = (Activity)activity.CreateCopy(false);
-                                currentActivity.ActivityName = "New - " + activity.ActivityName;
-                                bf.BusinessFlow.Activities.Insert(intIndex + 1, currentActivity);
-                                activity.Active = false;
-                                intIndex++;
-                                foreach (Act act in currentActivity.Acts.ToList())
+                                foreach (Act act in activity.Acts.ToList())
                                 {
                                     try
                                     {
-                                        if (act.Active && act.GetType() == typeof(ActWebAPIModel))
+                                        if (act.Active && (act.GetType() == typeof(ActWebAPIRest) || act.GetType() == typeof(ActWebAPISoap)))
                                         {
                                             // get the index of the action that is being converted 
-                                            int selectedActIndex = currentActivity.Acts.IndexOf(act);
+                                            int selectedActIndex = activity.Acts.IndexOf(act);
 
-                                            //pull pointed API Model
-                                            ApplicationAPIModel AAMB = WorkSpace.Instance.SolutionRepository.GetAllRepositoryItems<ApplicationAPIModel>().Where(x => x.Guid == ((ActWebAPIModel)act).APImodelGUID).FirstOrDefault();
-                                            if (AAMB == null)
+                                            //Create/Update API Model
+                                            ApplicationAPIModel applicationModel = GetAPIModelIfExists(act);
+
+                                            if (applicationModel == null && act.GetType() == typeof(ActWebAPIRest))
                                             {
-                                                act.Error = "Failed to find the pointed API Model";
-                                                act.ExInfo = string.Format("API Model with the GUID '{0}' was not found", ((ActWebAPIModel)act).APImodelGUID);
-                                                return;
+                                                applicationModel = GetWebAPIRestModel((ActWebAPIRest)act); 
+                                            }
+                                            else if(applicationModel == null && act.GetType() == typeof(ActWebAPISoap))
+                                            {
+                                                applicationModel = GetWebAPISOAPModel((ActWebAPISoap)act);
                                             }
 
-                                            //init matching real WebAPI Action
-                                            ActWebAPIBase actWebAPI = null;
-                                            if (AAMB.APIType == ApplicationAPIUtils.eWebApiType.REST)
+                                            if(applicationModel != null)
                                             {
-                                                actWebAPI = webAPIModel.CreateActWebAPIREST((ApplicationAPIModel)AAMB, (ActWebAPIModel)act);
-                                            }
-                                            else if (AAMB.APIType == ApplicationAPIUtils.eWebApiType.SOAP)
-                                            {
-                                                actWebAPI = webAPIModel.CreateActWebAPISOAP((ApplicationAPIModel)AAMB, (ActWebAPIModel)act);
-                                            }
 
-                                            // convert the old action
-                                            if (actWebAPI != null)
-                                            {
-                                                currentActivity.Acts.Insert(selectedActIndex + 1, actWebAPI);
-                                                currentActivity.Acts.Remove(act);
                                             }
                                         }
                                     }
@@ -84,7 +69,6 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib.ActionsConversion
                                         Reporter.ToLog(eLogLevel.ERROR, "Error occurred while trying to convert action", ex);
                                     }
                                 }
-                                currentActivity.TargetApplication = activity.TargetApplication;
                             }
                         } 
                     }
@@ -98,23 +82,58 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib.ActionsConversion
         }
 
         /// <summary>
+        /// This method is used to create or update API Model from Rest action
+        /// </summary>
+        /// <param name="act"></param>
+        /// <returns></returns>
+        private ApplicationAPIModel GetWebAPIRestModel(ActWebAPIRest act)
+        {
+            ApplicationAPIModel aPIModel = new ApplicationAPIModel();
+
+            return aPIModel;
+        }
+
+        /// <summary>
+        /// This method is used to create or update API Model from Soap action
+        /// </summary>
+        /// <param name="act"></param>
+        /// <returns></returns>
+        private ApplicationAPIModel GetWebAPISOAPModel(ActWebAPISoap act)
+        {
+            ApplicationAPIModel aPIModel = new ApplicationAPIModel();
+            string paramValuesXML = Convert.ToString(act.InputValues.Where(x => x.FileName == ActWebAPIBase.Fields.RequestBody).Select(x => x.Value).FirstOrDefault());
+            if (!string.IsNullOrEmpty(paramValuesXML))
+            {
+
+            }
+
+            return aPIModel;
+        }
+
+        /// <summary>
+        /// This method is used to check if the apimodel exists or not
+        /// </summary>
+        /// <param name="act"></param>
+        /// <returns></returns>
+        private ApplicationAPIModel GetAPIModelIfExists(Act act)
+        {
+            ApplicationAPIModel aPIModel = null;
+            string endPointURL = Convert.ToString(act.InputValues.Where(x => x.FileName == ActWebAPIBase.Fields.EndPointURL).Select(x => x.Value).FirstOrDefault());
+            if (!string.IsNullOrEmpty(endPointURL))
+            {
+                aPIModel = WorkSpace.Instance.SolutionRepository.GetAllRepositoryItems<ApplicationAPIModel>().Where(x => x.EndpointURL == endPointURL).FirstOrDefault();
+            }
+            return aPIModel;
+        }
+
+        /// <summary>
         /// This method will check if the businessFlow contains the WebService targetapplication
         /// </summary>
         /// <param name="bf"></param>
         /// <returns></returns>
         private bool IsValidWebServiceBusinessFlow(BusinessFlow bf)
         {
-            bool isValid = false;
-
-            foreach (var item in bf.TargetApplications)
-            {
-                if (item.ItemName.StartsWith("WebServices") || item.ItemName.Contains("WebServices"))
-                {
-                    isValid = true;
-                    break;
-                } 
-            }
-
+            bool isValid = string.IsNullOrEmpty(Convert.ToString(bf.TargetApplications.Where(x => x.ItemName.StartsWith(WEB_SERVICE) || x.ItemName.Contains(WEB_SERVICE)).FirstOrDefault())) ? false : true;
             return isValid;
         }
     }
