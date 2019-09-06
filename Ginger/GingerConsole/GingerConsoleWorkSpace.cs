fbfb24b55ce3dff8afbe6dd17a36646aa7410a26
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-06 09:20:05 -0500

    Fix GingerConsoleWorkSpace to set SCM Git

--- Ginger/GingerConsole/GingerConsoleWorkSpace.cs
+++ Ginger/GingerConsole/GingerConsoleWorkSpace.cs
@@ -17,11 +17,14 @@ limitations under the License.
 #endregion
 
 using amdocs.ginger.GingerCoreNET;
+using Amdocs.Ginger.Common;
+using Ginger.SolutionGeneral;
+using Ginger.SourceControl;
+using GingerCore.SourceControl;
+using GingerCoreNET.SourceControl;
 using System;
 // using GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.BusinessFlowLib;
-using GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib;
 using System.Collections.Generic;
-using Ginger.SolutionGeneral;
 
 namespace Amdocs.Ginger.GingerConsole
 {
@@ -39,8 +42,20 @@ namespace Amdocs.Ginger.GingerConsole
         }
 
         public void SetSolutionSourceControl(Solution solution, ref string repositoryRootFolder)
-        {
-            Console.WriteLine("SetSolutionSourceControl " + solution + " NOT IMPL !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        {            
+            SourceControlBase.eSourceControlType type = SourceControlIntegration.CheckForSolutionSourceControlType(solution.Folder, ref repositoryRootFolder);
+            if (type == SourceControlBase.eSourceControlType.GIT)
+            {
+                solution.SourceControl = new GITSourceControl();
+            }
+            else if (type == SourceControlBase.eSourceControlType.SVN)
+            {
+                // FIXME after SVN moved to .net core
+
+                // solution.SourceControl = new SVNSourceControl();
+                                
+                Reporter.ToConsole(eLogLevel.ERROR, "Source Control of type SVN is not yet supported in GingerConsole");
+            }
         }
 
         public void ShowBusinessFlows()
@@ -65,7 +80,7 @@ namespace Amdocs.Ginger.GingerConsole
 
         public void SolutionClosed()
         {
-            
+            Reporter.ToConsole(eLogLevel.INFO, "Solution Closed");
         }
     }
 }
