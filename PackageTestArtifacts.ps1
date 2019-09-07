f1a1c359c8798cf567349925845f22bcacb3cc69
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-06 22:35:55 -0500

    Azure yaml linux package more test artifacts

--- PackageTestArtifacts.ps1
+++ PackageTestArtifacts.ps1
@@ -6,9 +6,10 @@ New-Item -Path "/home/vsts/work/1/a" -Name "TestCompleted.txt" -ItemType "file" 
 Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerUtilsTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerUtilsTestArtifacts'
 Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerCoreCommonTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerCoreCommonTestArtifacts'
 Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerCoreNETUnitTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerCoreNETUnitTestArtifacts'
-# Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerConsoleTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerConsoleTestArtifacts'
-# Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerAutoPilotTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerAutoPilotTestArtifacts'
-# Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerPluginCoreTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerPluginCoreTestArtifacts'
+Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerPluginCoreTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerPluginCoreTestArtifacts'
+Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerConsoleTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerConsoleTestArtifacts'
+Compress-Archive -Path '/home/vsts/work/1/s/Ginger/GingerAutoPilotTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerAutoPilotTestArtifacts'
+
       
 # Compress-Archive -Path '/home/vsts/work/1/s/GingerCoreTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerCoreTestTestArtifacts'
 # Compress-Archive -Path '/home/vsts/work/1/s/GingerTest/bin/Release/netcoreapp2.2/TestArtifacts' -DestinationPath '/home/vsts/work/1/a/GingerTestArtifacts'
