40946b9c0919c2d493d38cd576ffe51de962013b
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-08 21:24:01 -0500

    Split CLIOptions to file per class

---
+++ Ginger/GingerCoreNET/RunLib/CLILib/RunOptions.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+
+namespace Amdocs.Ginger.CoreNET.RunLib.CLILib
+{
+
+
+
+    [Verb("run", HelpText = "execute run set")]
+    public class RunOptions : OptionsBase
+    {
+        public static string Verb => CLIOptionClassHelper.GetClassVerb<RunOptions>();
+
+        [Option('s', "solution", Required = true, HelpText = "Set solution folder")]
+        public string Solution { get; set; }
+
+
+        [Option('r', "runset", Default = "Default Run Set", Required = true, HelpText = "Set runset name")]
+        public string Runset { get; set; }
+
+        [Option('e', "env", Required = true, HelpText = "Set environment name")]
+        public string Environment { get; set; }
+
+        // causing issues so removing
+        //[Opton(longName: "env", Required = false, HelpText = "Set environment name same like using --environment")]
+        //public string Env { get { return Environment;  } set { Environment = value; } }
+
+
+        [Option('a', "analyze", Required = false, HelpText = "runAnalyzer")]
+        public bool RunAnalyzer { get; set; }
+
+        [Option('u', "showui", Required = false, Default = false, HelpText = "Show Auto Run Window UI - Windows only")]
+        public bool ShowAutoRunWindow { get; set; }
+
+        [Option('t', "artifacts-path", Required = false, HelpText = "Select Artifacts output directory")]
+        public string TestArtifactsPath { get; set; }
+    }
+}
---
+++ Ginger/GingerCoreNET/RunLib/CLILib/SCMOptions.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using GingerCoreNET.SourceControl;
+
+namespace Amdocs.Ginger.CoreNET.RunLib.CLILib
+{
+    [Verb("scm", HelpText = "Execute source control management operations")]
+    public class SCMOptions : OptionsBase
+    {
+        public static string Verb => CLIOptionClassHelper.GetClassVerb<SCMOptions>();
+
+        [Option('t', "type", Required = true, HelpText = "Source Control Management type i.e: GIT, SVN")]
+        public SourceControlBase.eSourceControlType SCMType { get; set; }
+
+        [Option('l', "url", Required = true, HelpText = "Source Control URL")]
+        public string URL { get; set; }
+
+        [Option('u', "user", Required = true, HelpText = "Source Control User")]
+        public string User { get; set; }
+
+        [Option('p', "pass", Required = true, HelpText = "Source Control Pass")]
+        public string Pass { get; set; }
+
+        [Option('e', "encrypted", Required = false, HelpText = "password is encrypted")]
+        public bool Encrypted { get; set; }
+
+        [Option('m', "encrypted-mechanism", Required = false, HelpText = "password decryption mechanism")]
+        public bool DecryptMechanism { get; set; }
+
+        public enum SCMOperation
+        {
+            Download,   // lower case!!!
+            GetLatest
+        }
+
+        [Option('o', "operation", Required = true, HelpText = "SCM Operation")]
+        public SCMOperation Operation { get; set; }
+
+    }
+}
