36e7130a4d39d6ecb5b2e27e764a6adb5c3246ee
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-10 20:50:50 -0500

    Prep for MongoDB to be plugin

---
+++ Ginger/MongoDB/PRogram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amdocs.Ginger.CoreNET.Drivers.CommunicationProtocol;
+using Amdocs.Ginger.Plugin.Core;
+using MongoDB;
+
+namespace GingerMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "MongoDb Database plugin";
+            Console.WriteLine("Starting MongoDb Database Plugin");
+
+            using (GingerNodeStarter gingerNodeStarter = new GingerNodeStarter())
+            {
+                if (args.Length > 0)
+                {
+                    gingerNodeStarter.StartFromConfigFile(args[0]);  // file name 
+                }
+                else
+                {
+                    gingerNodeStarter.StartNode("MSAccess Service 1", new MongoDbConnection(), SocketHelper.GetLocalHostIP(), 15001);
+                }
+                gingerNodeStarter.Listen();
+            }
+
+        }
+    }
+}
