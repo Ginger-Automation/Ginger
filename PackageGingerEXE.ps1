a1e02acab4a6f2d5961775c61e04ac8d3edca8cd
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-11 16:04:53 -0500

    Azure run CLI before pack log - windows

--- PackageGingerEXE.ps1
+++ PackageGingerEXE.ps1
@@ -3,22 +3,21 @@ Write-Host "-                    Package Ginger EXE                      "
 Write-Host "-------------------------------------------------------------"
 cd D:\a\1\s\Ginger\Ginger\bin\Release
 dir
-
 Compress-Archive -Path 'D:\a\1\s\Ginger\Ginger\bin\Release' -DestinationPath 'D:\a\1\a\GingerEXE'
-
-Write-Host "-------------------------------------------------------------"
-Write-Host "-                    Package Ginger.log                      "
-Write-Host "-------------------------------------------------------------"
-Compress-Archive -Path C:\Users\VssAdministrator\AppData\Roaming\amdocs\Ginger\WorkingFolder\Logs\Ginger_Log.txt -DestinationPath 'D:\a\1\a\GingerLog'
 
 
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                    Run CLI tests                           "
 Write-Host "-------------------------------------------------------------"
+cd D:\a\1\s\Ginger\Ginger\bin\Release
 $p = Start-Process Ginger.exe help
 $p.HasExited
 $p.ExitCode
 
+Write-Host "-------------------------------------------------------------"
+Write-Host "-                    Package Ginger.log                      "
+Write-Host "-------------------------------------------------------------"
+Compress-Archive -Path C:\Users\VssAdministrator\AppData\Roaming\amdocs\Ginger\WorkingFolder\Logs\Ginger_Log.txt -DestinationPath 'D:\a\1\a\GingerLog'
 
 
 Write-Host "-------------------------------------------------------------"
