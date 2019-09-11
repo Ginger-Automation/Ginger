ede0539fa43e6b72b2604d19a7b7f38f3bfb1e4f
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-11 13:09:16 -0500

    Package Ginger EXE

---
+++ PackageGingerEXE.ps1
@@ -0,0 +1,19 @@
+Write-Host "-------------------------------------------------------------"
+Write-Host "-                    Package Ginger EXE                      "
+Write-Host "-------------------------------------------------------------"
+
+Compress-Archive -Path 'D:\a\1\s\Ginger\Ginger\bin\release' -DestinationPath 'D:\a\1\a\GingerEXE'
+
+Write-Host "-------------------------------------------------------------"
+Write-Host "-                    Package Ginger.log                      "
+Write-Host "-------------------------------------------------------------"
+rem Compress-Archive -Path /home/vsts/.config/amdocs/Ginger/WorkingFolder/Logs/Ginger_Log.txt -DestinationPath '/home/vsts/work/1/a/GingerLog'
+
+
+Write-Host "-------------------------------------------------------------"
+Write-Host "-                         Artifacts List                    -"
+Write-Host "-------------------------------------------------------------"
+cd D:\a\1\a\
+dir
+
+exit $exitcode
