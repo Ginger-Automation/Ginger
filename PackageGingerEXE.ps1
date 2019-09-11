8ab4cf773c8db98c272cade39a8144b0dfbf9595
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-11 14:08:52 -0500

    Azure Check Ginger.log

--- PackageGingerEXE.ps1
+++ PackageGingerEXE.ps1
@@ -3,12 +3,14 @@ Write-Host "-                    Package Ginger EXE                      "
 Write-Host "-------------------------------------------------------------"
 
 Compress-Archive -Path 'D:\a\1\s\Ginger\Ginger\bin\Release' -DestinationPath 'D:\a\1\a\GingerEXE'
+dir D:\a\1\a\*.zip
 
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                    Package Ginger.log                      "
 Write-Host "-------------------------------------------------------------"
 
 cd D:\a\1\s\Ginger\Ginger\bin\Release
+dir
 Ginger.exe help
 # Compress-Archive -Path /home/vsts/.config/amdocs/Ginger/WorkingFolder/Logs/Ginger_Log.txt -DestinationPath '/home/vsts/work/1/a/GingerLog'
 
