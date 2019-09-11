6a79978b5b406ceb06044881b36389077b5344ca
Ambar <ambar_khan@live.com>
2019-09-11 18:58:22 +0530

    Spell Correct

--- Ginger/GingerPluginPlatforms/Platform/Web/Execution/WebPlatformActionHandler.cs
+++ Ginger/GingerPluginPlatforms/Platform/Web/Execution/WebPlatformActionHandler.cs
@@ -120,7 +120,7 @@ namespace Ginger.Plugin.Platform.Web.Execution
                     }
                     break;
 
-                case "WaitfUntilDisplay":
+                case ""WaitUntilDisapear"":
                     st.Reset();
                     st.Start();
 
