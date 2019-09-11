c92cd382e04b600a7cd5b426214f9212674f4cf4
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-11 13:37:00 -0500

    Package Ginger exe #2

--- PackageGingerEXE.ps1
+++ PackageGingerEXE.ps1
@@ -7,7 +7,7 @@ Compress-Archive -Path 'D:\a\1\s\Ginger\Ginger\bin\release' -DestinationPath 'D:
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                    Package Ginger.log                      "
 Write-Host "-------------------------------------------------------------"
-rem Compress-Archive -Path /home/vsts/.config/amdocs/Ginger/WorkingFolder/Logs/Ginger_Log.txt -DestinationPath '/home/vsts/work/1/a/GingerLog'
+# Compress-Archive -Path /home/vsts/.config/amdocs/Ginger/WorkingFolder/Logs/Ginger_Log.txt -DestinationPath '/home/vsts/work/1/a/GingerLog'
 
 
 Write-Host "-------------------------------------------------------------"
