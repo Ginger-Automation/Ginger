cb94cab268ad09120a5fccda947d9bd8375c9552
Yaron Weiss <yaron.weiss@amdocs.com>
2019-09-11 21:42:07 -0500

    More cleanup of warnings

--- Ginger/GingerCoreNET/ActionsLib/ActUIElement.cs
+++ Ginger/GingerCoreNET/ActionsLib/ActUIElement.cs
@@ -524,8 +524,7 @@ namespace GingerCore.Actions.Common
             [EnumValueDescription("Mouse Drag Drop")]
             MouseDragDrop,
         }
-
-        eElementType mElementType;
+        
        
         public eElementType ElementType
         {
