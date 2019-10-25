e350dbac30a216ac5c25565c4e49d78b795ef449
Yaron Weiss <yaron.weiss@amdocs.com>
2019-10-25 14:08:21 -0500

    IDatabase and IReporter

--- Ginger/GingerPluginCore/Database/IDatabase.cs
+++ Ginger/GingerPluginCore/Database/IDatabase.cs
@@ -16,26 +16,32 @@ limitations under the License.
 */
 #endregion
 
+using Amdocs.Ginger.Plugin.Core.Reporter;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
 
-namespace Amdocs.Ginger.CoreNET
+namespace Amdocs.Ginger.Plugin.Core.Database
 {
+
+    // Mark it as plugin interface so will be written to the services json
+    [GingerInterface("IDatabase", "Database Interface")]
     public interface IDatabase
     {
+        string ConnectionString { get; set; }
+
+        void InitReporter(IReporter reporter);
+        bool TestConnection();
+
+        string Name { get; }
         Boolean OpenConnection(Dictionary<string, string> parameters);
         void CloseConnection();
-        List<string> GetTablesList(string Name= null);// Keyspace - cassandra ??
+        List<string> GetTablesList(string Name= null);// Keyspace - Cassandra ??
         List<string> GetTablesColumns(string table);
         string RunUpdateCommand(string updateCmd, bool commit = true);
         string GetSingleValue(string Table, string Column, string Where);
-        DataTable DBQuery(string Query); //  int? timeout = null : TODO // Return Datatable 
-        int GetRecordCount(string Query);
-
-        //string GetConnectionString();
-        //bool MakeSureConnectionIsOpen();
-        //string GetMissingDLLErrorDescription();
+        DataTable DBQuery(string Query); //  int? timeout = null : TODO // Return Data table 
+        int GetRecordCount(string Query);        
     }
 }
