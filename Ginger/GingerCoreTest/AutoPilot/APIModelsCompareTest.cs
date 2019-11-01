6560d43d292ca702c51e074e1bd029198cd2ad9f
PraTeek Vyas <PraTeek.Vyas@amdocs.com>
2019-11-01 19:26:00 +0530

    API Comparison Tool - Unit Test

--- Ginger/GingerCoreTest/AutoPilot/APIModelsCompareTest.cs
+++ Ginger/GingerCoreTest/AutoPilot/APIModelsCompareTest.cs
@@ -1,5 +1,6 @@
 ﻿using amdocs.ginger.GingerCoreNET;
 using Amdocs.Ginger.Common;
+using Amdocs.Ginger.Common.Repository.ApplicationModelLib;
 using Amdocs.Ginger.CoreNET.Repository;
 using Amdocs.Ginger.Repository;
 using Ginger.Run;
@@ -29,7 +30,11 @@ namespace UnitTests.NonUITests.AutoPilot
     public class APIModelsCompareTest
     {
         static GingerRunner mGR = null;
-        static string wsdlURL = @"http://ws.cdyne.com/delayedstockquote/delayedstockquote.asmx?wsdl";
+
+        static List<string> xmlFiles;
+        static List<ApplicationAPIModel> learnedAPIsList;
+        static ObservableList<ApplicationAPIModel> existingAPIsList;
+        static int textExecutedCount = 0;
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext TC)
@@ -44,74 +49,137 @@ namespace UnitTests.NonUITests.AutoPilot
                 Directory.Delete(TempSolutionFolder, true);
             }
 
+            // Initialize XML Parser 
+            XMLTemplateParser xmlParser = new XMLTemplateParser();
+
+            // Initialize XML file names to be fetched from TestResources
+            xmlFiles = new List<string>()
+            {
+                @"XML\API_Templates_XML\Create_User.xml",
+                @"XML\API_Templates_XML\Delete_User.xml",
+                @"XML\API_Templates_XML\PhoneVerifySOAP_CheckPhoneNumber.xml",
+                @"XML\API_Templates_XML\Update_User.xml",
+                @"XML\API_Templates_XML\PhoneVerifySOAP_CheckPhoneNumbers.xml",
+                @"XML\API_Templates_XML\GetQuote_DelayedStockQuoteSoap.xml",
+            };
+
+            existingAPIsList = new ObservableList<ApplicationAPIModel>();
+
+            // Importing API Models from XML files (listed in xmlFiles)
+            foreach (String fileName in xmlFiles)
+            {
+                var tempFile = TestResources.GetTestResourcesFile(fileName);
+                existingAPIsList = xmlParser.ParseDocument(tempFile, existingAPIsList);
+                existingAPIsList.Last().EndpointURL = fileName;
+            };
+
+            // storing the learned API Models in Existing Models to keep intact while using for testing the Compaarison feature
+            learnedAPIsList = new List<ApplicationAPIModel>();
+            foreach (ApplicationAPIModel apiModel in existingAPIsList)
+            {
+                learnedAPIsList.Add(apiModel.CreateCopy() as ApplicationAPIModel);
+            }
+
             WorkSpace.Instance.SolutionRepository = GingerSolutionRepository.CreateGingerSolutionRepository();
             WorkSpace.Instance.SolutionRepository.CreateRepository(TempSolutionFolder);
 
             NewRepositorySerializer RS = new NewRepositorySerializer();
             NewRepositorySerializer.AddClassesFromAssembly(typeof(ApplicationAPIModel).Assembly);
             WorkSpace.Instance.SolutionRepository.Open(TempSolutionFolder);
-        }
 
-        [TestCleanup]
-        public void TestCleanUp()
-        {
+            // Modifying certain API Models for testing different Comparison status and scenarios
+            existingAPIsList[1].RequestBody = existingAPIsList[1].RequestBody + "This is modified";
+            existingAPIsList[3].RequestBody = existingAPIsList[3].RequestBody + "This is also modified";
 
-        }
-
-        public void APILearnWSDLDelayedStockQuote()
-        {
-            //Arrange
-            ObservableList<ApplicationAPIModel> apiModelsList = new ObservableList<ApplicationAPIModel>();
-
-            //Act
-            Learn(apiModelsList);
-            while (apiModelsList.Count < 6)
-            {
-                //Let it learn
-            }
-
-            apiModelsList[1].RequestBody = apiModelsList[1].RequestBody + "This is modified";
-            foreach (ApplicationAPIModel apiModel in apiModelsList.Take(2))
+            // Storing the API Models in the Solution Repository as will be utilized during Comparison process
+            foreach (ApplicationAPIModel apiModel in existingAPIsList.Skip(1).Take(4))
             {
                 WorkSpace.Instance.SolutionRepository.AddRepositoryItem(apiModel);
             }
         }
 
-        private async void Learn(ObservableList<ApplicationAPIModel> AAMSList)
+        [TestCleanup]
+        public void TestCleanUp()
         {
-            WSDLParser wsdlParser = new WSDLParser();
-            await Task.Run(() => wsdlParser.ParseDocument(wsdlURL, AAMSList));
+            if (textExecutedCount == 3)
+            {
+                xmlFiles.Clear();
+                learnedAPIsList.Clear();
+                existingAPIsList.Clear();
+                textExecutedCount = 0;
+            }
+            else
+                textExecutedCount++;
         }
 
         [TestMethod]
         [Timeout(60000)]
-        public void APIComparisonWSDLDelayedStockQuoteTest()
+        public void APIComparison_MatchingTest()
         {
-            APILearnWSDLDelayedStockQuote();
+            // Arrange
+            ObservableList<ApplicationAPIModel> learnedModels = new ObservableList<ApplicationAPIModel>(learnedAPIsList);
 
-            ObservableList<ApplicationAPIModel> learnedAPIsList = new ObservableList<ApplicationAPIModel>();
-            Learn(learnedAPIsList);
-            while(learnedAPIsList.Count < 6)
-            {
-                // Let the learning finish
-            }
-
-            ObservableList<DeltaAPIModel> deltaAPIsList = APIDeltaUtils.DoAPIModelsCompare(learnedAPIsList);
+            //Act
+            ObservableList<DeltaAPIModel> deltaAPIsList = APIDeltaUtils.DoAPIModelsCompare(learnedModels);
 
             //Assert - Check Count
-            Assert.AreEqual(deltaAPIsList.Count, 6, "Is Delta Models counts to 6");
+            Assert.AreEqual(deltaAPIsList.Count, 6, "Is Delta Models count is 6");
+
+            Assert.AreEqual(deltaAPIsList.Where(d => d.matchingAPIModel != null).Count(), 4, "Confirm if 4 API Models already exists in the solution.");
+        }
+
+        [TestMethod]
+        [Timeout(60000)]
+        public void New_APIComparisonStatusTest()
+        {
+            // Arrange
+            ObservableList<ApplicationAPIModel> learnedModels = new ObservableList<ApplicationAPIModel>() { learnedAPIsList[0], learnedAPIsList[5] };
+
+            //Act
+            ObservableList<DeltaAPIModel> deltaAPIsList = APIDeltaUtils.DoAPIModelsCompare(learnedModels);
+
+            // Assert - Confirm Default Selected Operation = Add New, for API with Comparison Status = New
+            Assert.AreEqual(deltaAPIsList[0].comparisonStatus, DeltaAPIModel.eComparisonOutput.New, deltaAPIsList[0].learnedAPI.Name + " is NEW API.");
+            Assert.AreEqual(deltaAPIsList[0].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.Add, deltaAPIsList[0].learnedAPI.Name + " is New API and thus, Default Selected Operation is 'Add New'.");
+
+            Assert.AreEqual(deltaAPIsList[1].comparisonStatus, DeltaAPIModel.eComparisonOutput.New, deltaAPIsList[1].learnedAPI.Name + " is NEW API.");
+            Assert.AreEqual(deltaAPIsList[1].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.Add, deltaAPIsList[1].learnedAPI.Name + " is New API and thus, Default Selected Operation is 'Add New'.");
+        }
+
+        [TestMethod]
+        [Timeout(60000)]
+        public void Modified_APIComparisonStatusTest()
+        {
+            // Arrange
+            ObservableList<ApplicationAPIModel> learnedModels = new ObservableList<ApplicationAPIModel>() { learnedAPIsList[1], learnedAPIsList[3] };
+
+            //Act
+            ObservableList<DeltaAPIModel> deltaAPIsList = APIDeltaUtils.DoAPIModelsCompare(learnedModels);
+
+            // Assert - Confirm Default Selected Operation = Replace Existing with New, for API with Comparison Status = Modified
+            Assert.AreEqual(deltaAPIsList[0].comparisonStatus, DeltaAPIModel.eComparisonOutput.Modified, deltaAPIsList[0].learnedAPI.Name + " is Modified and having modified RequestBody.");
+            Assert.AreEqual(deltaAPIsList[0].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.ReplaceExisting, deltaAPIsList[0].learnedAPI.Name + " is Modified and thus Default Selected Operation is 'Replace Existing with New'.");
+
+            Assert.AreEqual(deltaAPIsList[1].comparisonStatus, DeltaAPIModel.eComparisonOutput.Modified, deltaAPIsList[1].learnedAPI.Name + " is Modified and having modified RequestBody.");
+            Assert.AreEqual(deltaAPIsList[1].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.ReplaceExisting, deltaAPIsList[1].learnedAPI.Name + " is Modified and thus Default Selected Operation is 'Replace Existing with New'.");
+        }
+
+        [TestMethod]
+        [Timeout(60000)]
+        public void Unchanged_APIComparisonStatusTest()
+        {
+            // Arrange
+            ObservableList<ApplicationAPIModel> learnedModels = new ObservableList<ApplicationAPIModel>() { learnedAPIsList[2], learnedAPIsList[4] };
+
+            //Act
+            ObservableList<DeltaAPIModel> deltaAPIsList = APIDeltaUtils.DoAPIModelsCompare(learnedModels);
 
             // Assert - Confirm Default Selected Operation = Do Not Add, for API with Comparison Status = UnChanged
             Assert.AreEqual(deltaAPIsList[0].comparisonStatus, DeltaAPIModel.eComparisonOutput.Unchanged, deltaAPIsList[0].learnedAPI.Name + " is UnChanged.");
             Assert.AreEqual(deltaAPIsList[0].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.DoNotAdd, deltaAPIsList[0].learnedAPI.Name + " is UnChanged and thus Default Selected Operation is 'Do Not Add'.");
 
-            // Assert - Confirm Default Selected Operation = Replace Existing with New, for API with Comparison Status = Modified
-            Assert.AreEqual(deltaAPIsList[1].comparisonStatus, DeltaAPIModel.eComparisonOutput.Modified, deltaAPIsList[1].learnedAPI.Name + " is Modified and having modified RequestBody.");
-            Assert.AreEqual(deltaAPIsList[1].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.ReplaceExisting , deltaAPIsList[1].learnedAPI.Name + " is Modified and thus Default Selected Operation is 'Replace Existing with New'.");
-
-            // Assert - Confirm Default Selected Operation = Add New, for API with Comparison Status = New
-            Assert.AreEqual(deltaAPIsList[2].comparisonStatus, DeltaAPIModel.eComparisonOutput.New, deltaAPIsList[2].learnedAPI.Name + " is NEW API.");
-            Assert.AreEqual(deltaAPIsList[2].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.Add, deltaAPIsList[2].learnedAPI.Name + " is New API and thus, Default Selected Operation is 'Add New'.");
+            Assert.AreEqual(deltaAPIsList[1].comparisonStatus, DeltaAPIModel.eComparisonOutput.Unchanged, deltaAPIsList[1].learnedAPI.Name + " is UnChanged.");
+            Assert.AreEqual(deltaAPIsList[1].SelectedOperationEnum, DeltaAPIModel.eHandlingOperations.DoNotAdd, deltaAPIsList[1].learnedAPI.Name + " is UnChanged and thus Default Selected Operation is 'Do Not Add'.");
         }
     }
 }
