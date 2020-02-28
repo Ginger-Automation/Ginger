db54655c11181aec9ab17a4370c063dae2eddb4a
Arvind Ghodse <Arvind.Ghodse@amdocs.com>
2020-02-28 19:45:08 +0530

    Updated GingerDicser in the missing files

--- Ginger/Ginger/Actions/ActionConversion/ActionsConversionWizard.cs
+++ Ginger/Ginger/Actions/ActionConversion/ActionsConversionWizard.cs
@@ -86,11 +86,11 @@ namespace Ginger.Actions.ActionConversion
 
             if (ConversionType == eActionConversionType.MultipleBusinessFlow)
             {
-                AddPage(Name: "Select Business Flow's for Conversion", Title: "Select Business Flow's for Conversion", SubTitle: "Select Business Flow's for Conversion", Page: new SelectBusinessFlowWzardPage(mListOfBusinessFlow, Context));
+                AddPage(Name: "Select Business Flow's for Conversion", Title: "Select " + GingerDicser.GetTermResValue(eTermResKey.BusinessFlow) + "'s for Conversion", SubTitle: "Select " + GingerDicser.GetTermResValue(eTermResKey.BusinessFlow) + "'s for Conversion", Page: new SelectBusinessFlowWzardPage(mListOfBusinessFlow, Context));
             }
             else if (ConversionType == eActionConversionType.SingleBusinessFlow)
             {
-                AddPage(Name: "Select Activities for Conversion", Title: "Select Activities for Conversion", SubTitle: "Select Activities for Conversion", Page: new SelectActivityWzardPage());
+                AddPage(Name: "Select Activities for Conversion", Title: "Select " + GingerDicser.GetTermResValue(eTermResKey.Activities) + " for Conversion", SubTitle: "Select " + GingerDicser.GetTermResValue(eTermResKey.Activities) + " for Conversion", Page: new SelectActivityWzardPage());
             }
 
             AddPage(Name: "Select Legacy Actions Type for Conversion", Title: "Select Legacy Actions Type for Conversion", SubTitle: "Select Legacy Actions Type for Conversion", Page: new SelectActionWzardPage());
