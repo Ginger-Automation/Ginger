435ef7b72836ab4cf05164b7287a45a6bb6937ea
Ravi Kumar <RaviKumar.Gupta@amdocs.com>
2020-04-06 12:30:40 +0530

    Adding support of variable type Number with validation

---
+++ Ginger/Ginger/Variables/VariableEditPages/VariableNumber.xaml.cs
@@ -0,0 +1,85 @@
+﻿#region License
+/*
+Copyright © 2014-2020 European Support Limited
+
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at 
+
+http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. 
+*/
+#endregion
+using GingerCore.Variables;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace Ginger.Variables
+{
+    /// <summary>
+    /// Interaction logic for VariableNumber.xaml
+    /// </summary>
+    public partial class VariableNumberPage : Page
+    {
+        public VariableNumberPage(VariableNumber varNumber)
+        {
+            InitializeComponent();
+            GingerCore.GeneralLib.BindingHandler.ObjFieldBinding(txtNumberValue, TextBox.TextProperty, varNumber, nameof(VariableNumber.InitialNumberValue));
+            GingerCore.GeneralLib.BindingHandler.ObjFieldBinding(rdoInputInt,RadioButton.IsCheckedProperty ,varNumber, nameof(VariableNumber.IsIntegerValue));
+            GingerCore.GeneralLib.BindingHandler.ObjFieldBinding(txtMinValue, TextBox.TextProperty, varNumber, nameof(VariableNumber.MinValue));
+            GingerCore.GeneralLib.BindingHandler.ObjFieldBinding(txtMaxValue, TextBox.TextProperty, varNumber, nameof(VariableNumber.MaxValue));
+            GingerCore.GeneralLib.BindingHandler.ObjFieldBinding(txtPrecisionValue, TextBox.TextProperty, varNumber, nameof(VariableNumber.PrecisionValue));
+            SetNummericType();
+        }
+
+        private void SetNummericType()
+        {
+            if(new VariableNumber().IsIntegerValue)
+            {
+                rdoInputInt.IsChecked = true;
+            }
+            else
+            {
+                rdoInputDecimal.IsChecked = true;
+                pnlPrecision.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        private void rdoInputDecimal_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            pnlPrecision.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void rdoInputInt_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            pnlPrecision.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        //private void txtNumberValue_LostFocus(object sender, System.Windows.RoutedEventArgs e)
+        //{
+        //  var variableNumber =  new VariableNumber();
+        //    double retNum;
+        //    bool isNum = Double.TryParse(txtNumberValue.Text, System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+        //    if(isNum)
+        //    {
+        //        variableNumber.InitialNumberValue = Convert.ToDouble(txtNumberValue.Text);
+        //    }
+        //    else
+        //    {
+        //        e.Handled = false;
+        //    }
+            
+        //}
+        //private void txtNumberValue_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    Regex regex = new Regex("[^0-9]+");
+        //    e.Handled = regex.IsMatch(txtNumberValue.Text);
+        //}
+    }
+}
---
+++ Ginger/GingerCoreCommon/VariablesLib/VariableNumber.cs
@@ -0,0 +1,209 @@
+﻿#region License
+/*
+Copyright © 2014-2020 European Support Limited
+
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at 
+
+http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. 
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Amdocs.Ginger.Common;
+using Amdocs.Ginger.Common.Enums;
+using Amdocs.Ginger.Repository;
+
+namespace GingerCore.Variables
+{
+    public class VariableNumber : VariableBase
+    {
+        private double mInitialNumberValue;
+
+        private double mMinValue;
+        private double mMaxValue;
+
+        private int mPrecisionValue;
+
+        private bool mIsIntegerValue;
+        
+        [IsSerializedForLocalRepository]
+        public bool IsIntegerValue
+        {
+            set
+            {
+                mIsIntegerValue = value;
+                if (IsIntegerValue)
+                {
+                    mMinValue = Int32.MinValue;
+                    mMaxValue = Int32.MaxValue;
+                }
+                else
+                {
+                    mMinValue = float.MinValue;
+                    mMaxValue = float.MaxValue;
+                    mPrecisionValue = 2; //default
+                }
+                mInitialNumberValue = MinValue;
+            }
+            get
+            {
+                return mIsIntegerValue;
+            }
+        }
+
+        [IsSerializedForLocalRepository]
+        public double MinValue
+        {
+            get 
+            {
+                return mMinValue; 
+            }
+            set 
+            {
+                mMinValue = value;
+                OnPropertyChanged("MinValue");
+                OnPropertyChanged("Formula");
+            }
+        }
+
+        [IsSerializedForLocalRepository]
+        public double MaxValue
+        {
+            get 
+            { 
+                return mMaxValue;
+            }
+            set
+            { 
+                mMaxValue = value;
+                OnPropertyChanged("MaxValue");
+                OnPropertyChanged("Formula");
+            }
+        }
+
+        public VariableNumber() { }
+        public override string VariableType
+        {
+            get { return "Number"; }
+        }
+
+        public override string VariableEditPage
+        {
+            get
+            {
+                return "VariableNumberPage";
+            }
+        }
+        public override eImageType Image
+        {
+            get { return eImageType.SequentialExecution; }
+        }
+
+        public override bool SupportResetValue
+        {
+            get { return true; }
+        }
+
+        public override bool SupportAutoValue
+        {
+            get { return true; }
+        }
+
+        public override bool SupportSetValue
+        {
+            get { return true; }
+        }
+
+        public override string VariableUIType
+        {
+            get { return GingerDicser.GetTermResValue(eTermResKey.Variable) + " Number"; }
+        }
+
+        [IsSerializedForLocalRepository]
+        public double InitialNumberValue
+        {
+            set
+            {
+                mInitialNumberValue = value;
+                Value = value.ToString();
+                OnPropertyChanged("InitialNumberValue");
+                OnPropertyChanged("Formula");
+            }
+            get
+            {
+                return mInitialNumberValue;
+            }
+        }
+
+        [IsSerializedForLocalRepository]
+        public int PrecisionValue
+        {
+            get
+            {
+                if(mPrecisionValue ==0)
+                {
+                    mPrecisionValue = 2;
+                }
+                return mPrecisionValue;
+            }
+            set
+            {
+                mPrecisionValue = value;
+                OnPropertyChanged("Formula");
+                OnPropertyChanged("PrecisionValue");
+                OnPropertyChanged("InitialNumberValue");
+            }
+        }
+
+        public override void GenerateAutoValue()
+        {
+
+        }
+
+        public override string GetFormula()
+        {
+            var isIntegerNumber = IsNumericValue(mInitialNumberValue);
+            if (isIntegerNumber)
+            {
+                return "Initial Value=" + mInitialNumberValue;
+            }
+            else
+            {
+                //var setPrecision = new NumberFormatInfo();
+                //setPrecision.NumberDecimalDigits = PrecisionValue;
+                mInitialNumberValue = Math.Round(mInitialNumberValue, PrecisionValue);
+                return "Initial Value=" + mInitialNumberValue;//mInitialNumberValue.ToString("N", setPrecision);
+            }
+
+        }
+
+        private bool IsNumericValue(object inputValue)
+        {
+            double retNum;
+            bool isNum = Double.TryParse(Convert.ToString(inputValue), System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum;
+        }
+
+        public override List<eSetValueOptions> GetSupportedOperations()
+        {
+            var supportedOperations = new List<VariableBase.eSetValueOptions>();
+            supportedOperations.Add(eSetValueOptions.SetValue);
+            supportedOperations.Add(eSetValueOptions.ResetValue);
+            return supportedOperations;
+        }
+
+        public override void ResetValue()
+        {
+            Value = mInitialNumberValue.ToString();
+        }
+    }
+}
