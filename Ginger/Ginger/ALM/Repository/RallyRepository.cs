987efcab3206edfc4c8b9814b4070ce1abc3c51d
Jinendra Ghodke <JGHODKE@amdocs.com>
2020-10-26 21:43:40 +0530

    ALM Import to correct BF sub folder Fix

--- Ginger/Ginger/ALM/Repository/RallyRepository.cs
+++ Ginger/Ginger/ALM/Repository/RallyRepository.cs
@@ -137,7 +137,7 @@ namespace Ginger.ALM.Repository
                         }
 
                         //save bf
-                        WorkSpace.Instance.SolutionRepository.AddRepositoryItem(tsBusFlow);
+                        AddTestSetFlowToFolder(tsBusFlow, importDestinationPath);
                         Reporter.HideStatusMessage();
                     }
                     catch (Exception ex)
