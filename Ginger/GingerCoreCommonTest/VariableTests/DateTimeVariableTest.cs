1dcf19dbaabd30ccd9922615fb6bf68ca5d1c6b8
Ravi Kumar <RaviKumar.Gupta@amdocs.com>
2021-01-07 09:48:56 +0530

    Fixing unit test

--- Ginger/GingerCoreCommonTest/VariableTests/DateTimeVariableTest.cs
+++ Ginger/GingerCoreCommonTest/VariableTests/DateTimeVariableTest.cs
@@ -104,12 +104,12 @@ namespace GingerCoreCommonTest.VariableTests
             var variableDateTime = new VariableDateTime();
             variableDateTime.Name = "test";
             variableDateTime.Value = "123";
-            variableDateTime.InitialDateTime = "01-Jan-2020";
+            variableDateTime.InitialDateTime = "01-Jan-2021";
             //Act
             string formulaStr = variableDateTime.GetFormula();
 
             //Assert
-            Assert.AreEqual(@"Initial DateTime : 01-Jan-2020", formulaStr, "Mismatch with Default Formula String");
+            Assert.AreEqual(@"Initial DateTime : 01-Jan-2021", formulaStr, "Mismatch with Default Formula String");
             Assert.AreEqual(variableDateTime.InitialDateTime, variableDateTime.Value);
         }
 
