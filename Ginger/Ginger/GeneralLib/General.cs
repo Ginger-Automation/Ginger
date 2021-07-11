6fb5cd9ef0c3e156f11d913c3c062d130cb4c0dd
Meni Kadosh <MENIK@amdocs.com>
2021-07-11 22:47:15 +0300

    Shift to offline and online help lib view- part 2

--- Ginger/Ginger/GeneralLib/General.cs
+++ Ginger/Ginger/GeneralLib/General.cs
@@ -38,8 +38,6 @@ namespace Ginger
 {
     public static class General
     {
-        static GingerWebHelpPage mGingerHelpWindow = null;
-
         public enum eRIPageViewMode
         {
             /// <summary>
@@ -126,15 +124,7 @@ namespace Ginger
 
         public static void ShowGingerHelpWindow(string SearchString="")
         {
-            if (mGingerHelpWindow == null)
-                mGingerHelpWindow = new GingerWebHelpPage();
-            //else  //TODO:implement Close
-            //{
-            //    mGingerHelpWindow.Close();
-            //    mGingerHelpWindow = new HelpWindow(SearchString);
-            //}
-
-            mGingerHelpWindow.ShowAsWindow();            
+            GingerHelpProvider.ShowHelpLibrary(SearchString);
         }
 
         internal static string SetupBrowseFile(OpenFileDialog dlg, bool isRelativePath = true)
--- Ginger/Ginger/Help/GingerHelpProvider.cs
+++ Ginger/Ginger/Help/GingerHelpProvider.cs
@@ -16,7 +16,14 @@ limitations under the License.
 */
 #endregion
 
+using Amdocs.Ginger.Common;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace Ginger.Help
@@ -60,6 +67,73 @@ namespace Ginger.Help
         public static void SetHelpString(DependencyObject obj, string value)
         {
             obj.SetValue(HelpStringProperty, value);
+        }
+
+        public static void ShowHelpLibrary(string searchText)
+        {
+            if (!ShowOfflineHelpLibrary(searchText))
+            {
+                ShowOnlineHelpLibrary(searchText);
+            }
+        }
+
+        public static bool ShowOfflineHelpLibrary(string searchText, bool silent=true)
+        {
+            try
+            {
+                string helpLibIndex = System.IO.Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Help", "index.html");
+                if (File.Exists(helpLibIndex))
+                {
+                    //start local help lib
+                    System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                    proc.StartInfo.FileName = helpLibIndex;
+                    if (!string.IsNullOrEmpty(searchText))
+                    {
+                        proc.StartInfo.Arguments = "?rhsearch=" + Uri.EscapeUriString(searchText); //TODO: not working, need to fix
+                    }
+                    proc.Start();
+                }
+                else
+                {
+                    if (!silent)
+                    {
+                        Reporter.ToLog(eLogLevel.WARN, "Offline help library was not found");
+                    }
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Reporter.ToUser(eUserMsgKey.StaticErrorMessage, string.Format("Error Ocurred while loading offline help library, error: {0}", ex.Message));
+                Reporter.ToLog(eLogLevel.ERROR, "Error Ocurred while loading help library", ex);
+                return false;
+            }
+        }
+
+        public static void ShowOnlineHelpLibrary(string searchText)
+        {
+            try
+            {
+
+                string publicLibURI = @"https://ginger-automation.github.io/Ginger-Web-Help";
+
+                if (!string.IsNullOrEmpty(searchText))
+                {
+                    System.Diagnostics.Process.Start(publicLibURI + "/?rhsearch=" + Uri.EscapeUriString(searchText));
+                }
+                else
+                {
+                    System.Diagnostics.Process.Start(publicLibURI);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Reporter.ToUser(eUserMsgKey.StaticErrorMessage, string.Format("Error Ocurred while loading online help library, error: {0}", ex.Message));
+                Reporter.ToLog(eLogLevel.ERROR, "Error Ocurred while loading help library", ex);
+            }
         }
     }
 }
