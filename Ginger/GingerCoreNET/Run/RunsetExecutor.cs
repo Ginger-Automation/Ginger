2dcb15a09ec8eb11470bf3ad6491e4917dc53497
Sudarshan Sathe <sudarshan.sathe@amdocs.com>
2021-08-05 22:42:29 +0530

    adding runner and runset start,end time while execution and removing from accountreport

--- Ginger/GingerCoreNET/Run/RunsetExecutor.cs
+++ Ginger/GingerCoreNET/Run/RunsetExecutor.cs
@@ -417,6 +417,7 @@ namespace Ginger.Run
                 //Process all pre execution Run Set Operations
                 if (doContinueRun == false)
                 {
+                    RunSetConfig.StartTimeStamp = DateTime.UtcNow;
                     Reporter.ToLog(eLogLevel.INFO, string.Format("Running Pre-Execution {0} Operations", GingerDicser.GetTermResValue(eTermResKey.RunSet)));
                     WorkSpace.Instance.RunsetExecutor.ProcessRunSetActions(new List<RunSetActionBase.eRunAt> { RunSetActionBase.eRunAt.ExecutionStart, RunSetActionBase.eRunAt.DuringExecution });
                 }
@@ -551,6 +552,7 @@ namespace Ginger.Run
             }
             finally
             {
+                RunSetConfig.EndTimeStamp = DateTime.UtcNow;
                 mRunSetConfig.IsRunning = false;
             }
         }
