078107f9b01dc30ec579d673f4e70f87171d9138
Rajdeep Mukherjee <rajdeemu@amdocs.com>
2021-10-07 13:27:04 +0530

    Removed extra string variable and assigned directly

--- Ginger/Ginger/ApplicationModelsLib/POMModels/POMEditPage.xaml.cs
+++ Ginger/Ginger/ApplicationModelsLib/POMModels/POMEditPage.xaml.cs
@@ -104,8 +104,7 @@ namespace Ginger.ApplicationModelsLib.POMModels
             xFrameBusinessFlowControl.Content = mBusinessFlowControl;
 
             xShowIDUC.Init(mPOM);
-            string expanderHeaderName = string.Format("'{0}' Details", mPOM.Name);
-            xFirstRowExpanderLabel.Content = expanderHeaderName;
+            xFirstRowExpanderLabel.Content = string.Format("'{0}' Details", mPOM.Name);
             GingerCore.GeneralLib.BindingHandler.ObjFieldBinding(xNameTextBox, TextBox.TextProperty, mPOM, nameof(mPOM.Name));
             GingerCore.GeneralLib.BindingHandler.ObjFieldBinding(xDescriptionTextBox, TextBox.TextProperty, mPOM, nameof(mPOM.Description));
             xPageURLTextBox.Init(null, mPOM, nameof(mPOM.PageURL));
