f7e935017b536e8b9b63b12ccd95a4eabb88cf3a
Haim Uzan <uzanhaim@gmail.com>
2021-10-07 11:58:19 +0300

    Fix comma and equal issues

--- Ginger/GingerCoreNET/Run/Excels/ExcelNPOIOperations.cs
+++ Ginger/GingerCoreNET/Run/Excels/ExcelNPOIOperations.cs
@@ -132,38 +132,6 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib
             }
             return true;
         }
-
-        private List<Tuple<string, object>> GetSetDataUsed(string setDataUsed)
-        {
-            List<Tuple<string, object>> columnNameAndValue = new List<Tuple<string, object>>();
-            if (String.IsNullOrEmpty(setDataUsed))
-            {
-                return columnNameAndValue;
-            }
-            bool isError = false;
-            string result = System.Text.RegularExpressions.Regex.Replace(setDataUsed, @",(?=[^']*'(?:[^']*'[^']*')*[^']*$)", "~^GINGER-EXCEL-COMMA-REPLACE^~");
-            string[] varColMaps = result.Split(',');
-            varColMaps.ToList().ForEach(c =>
-            {
-                result = System.Text.RegularExpressions.Regex.Replace(c, @"=(?=[^']*'(?:[^']*'[^']*')*[^']*$)", "~^GINGER-EXCEL-EQUAL-REPLACE^~");
-                string[] setData = result.Split('=');
-                if (setData.Length == 2)
-                {
-                    string rowToSet = setData[0].Replace("[", "").Replace("]", "");
-                    object valueToSet = setData[1].Replace("'", "").Replace("~^GINGER-EXCEL-COMMA-REPLACE^~", ",")
-                                                   .Replace("~^GINGER-EXCEL-EQUAL-REPLACE^~", "=");
-                    columnNameAndValue.Add(new Tuple<string, object>(rowToSet, valueToSet));
-                }
-                else
-                {
-                    Reporter.ToLog(eLogLevel.INFO, "Invalid data added to 'SetDataUsed' text box");
-                    isError = true;
-                }
-
-            });
-            return isError ? null : columnNameAndValue;
-        }
-
         private DataTable GetFilteredDataTable(DataTable dataTable, bool selectAllRows)
         {
             return selectAllRows ? dataTable.DefaultView.ToTable() : dataTable.DefaultView.ToTable().AsEnumerable().Take(1).CopyToDataTable();
@@ -188,9 +156,9 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib
             return workbook;
         }
 
-        public bool UpdateExcelData(string fileName, string sheetName, string filter, string setDataUsed, string primaryKey = null, string key = null)
+        public bool UpdateExcelData(string fileName, string sheetName, string filter, List<Tuple<string, object>> updateCellValuesList, string primaryKey = null, string key = null)
         {
-            UpdateCellList = GetSetDataUsed(setDataUsed);
+            UpdateCellList.AddRange(updateCellValuesList);
             if (UpdateCellList.Count > 0)
             {
                 var headerRow = mSheet.GetRow(0);
@@ -306,8 +274,6 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib
 
         public bool WriteData(string fileName, string sheetName, string filter, string setDataUsed, List<Tuple<string, object>> updateCellValuesList, string primaryKey = null, string key = null)
         {
-            UpdateCellList = GetSetDataUsed(setDataUsed);
-            UpdateCellList.AddRange(updateCellValuesList);
             if (!String.IsNullOrWhiteSpace(primaryKey))
             {
                 if (string.IsNullOrWhiteSpace(filter))
@@ -319,14 +285,14 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib
                     filter = $"({filter}) and ({primaryKey})";
                 }
             }
-            return UpdateCellsData(UpdateCellList, mExcelDataTable, filter, fileName);
+            return UpdateCellsData(updateCellValuesList, mExcelDataTable, filter, fileName);
         }
 
         private bool UpdateCellsData(List<Tuple<string, object>> updateCellList, DataTable mExcelDataTable, string filter, string fileName)
         {
-            if (UpdateCellList.Count > 0)
+            if (updateCellList.Count > 0)
             {
-                foreach (var cell in UpdateCellList)
+                foreach (var cell in updateCellList)
                 {
                     int columnIndex = mExcelDataTable.Columns[cell.Item1.Replace("[", "").Replace("]", "").Trim()].Ordinal;
                     List<DataRow> filteredList = mExcelDataTable.Select(filter).ToList();
