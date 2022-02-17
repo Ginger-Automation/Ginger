78e57f25b660c5dc0fc3a3f5b9c0a0d60179974f
Sudarshan Sathe <sudarshan.sathe@amdocs.com>
2022-02-17 19:55:15 +0530

    simplifying path to avoid path too long exception

--- Ginger/GingerCoreNET/Run/RunSetActions/RunSetActionHTMLReportSendEmailOperations.cs
+++ Ginger/GingerCoreNET/Run/RunSetActions/RunSetActionHTMLReportSendEmailOperations.cs
@@ -85,20 +85,23 @@ namespace Ginger.Run.RunSetActions
                 RunSetActionHTMLReportSendEmail.Email.EmailOperations.alternateView = null;
                 LiteDbRunSet liteDbRunSet = null;
                 var loggerMode = WorkSpace.Instance.Solution.LoggerConfigurations.SelectedDataRepositoryMethod;
-
+                const int MAXPATHLENGTH = 150;                
                 if (loggerMode == ExecutionLoggerConfiguration.DataRepositoryMethod.LiteDB)
                 {
                     //Reporter.ToLog(eLogLevel.DEBUG, "Run set operation send Email: Using LiteDB and using new WebReportGenerator");
                     reportsResultFolder = Path.Combine(ExtensionMethods.GetReportDirectory(currentConf.HTMLReportsFolder), "Reports");
-                    string reportTempPath = Path.Combine(reportsResultFolder, $"{General.RemoveInvalidFileNameChars(WorkSpace.Instance.RunsetExecutor.RunSetConfig.Name)}_{DateTime.UtcNow.ToString("yyyymmddhhmmssfff")}");
-                    if (reportTempPath.Length >255)
+                    string RunsetName = General.RemoveInvalidFileNameChars(WorkSpace.Instance.RunsetExecutor.RunSetConfig.Name);
+                    string DateTimeStamp = DateTime.UtcNow.ToString("yyyymmddhhmmssfff");                    
+                    int remainingChars = MAXPATHLENGTH - (reportsResultFolder.Length + DateTimeStamp.Length);
+                    if(remainingChars > 0 && RunsetName.Length > remainingChars)
                     {
-                        reportsResultFolder = Path.Combine(reportsResultFolder, $"{General.RemoveInvalidFileNameChars(WorkSpace.Instance.RunsetExecutor.RunSetConfig.Name.Substring(0,5))}_{DateTime.UtcNow.ToString("yyyymmddhhmmssfff")}");
+                        reportsResultFolder = Path.Combine(reportsResultFolder, $"{RunsetName.Substring(0, remainingChars)}_{DateTimeStamp}");
                     }
                     else
                     {
-                        reportsResultFolder = reportTempPath;
-                    }                   
+                        reportsResultFolder = Path.Combine(reportsResultFolder, $"{RunsetName}_{DateTimeStamp}");
+                    }
+                                      
                     WebReportGenerator webReporterRunner = new WebReportGenerator();
                     liteDbRunSet = webReporterRunner.RunNewHtmlReport(reportsResultFolder, null, null, false);
                 }
