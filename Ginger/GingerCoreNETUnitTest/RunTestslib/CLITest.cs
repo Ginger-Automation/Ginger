50baf949ab88a78e815810a8fbf3ef73b6f4a19b
Mahesh Kale <MKALE@amdocs.com>
2022-06-29 01:20:25 +0530

    changed password in CLITest

--- Ginger/GingerCoreNETUnitTest/RunTestslib/CLITest.cs
+++ Ginger/GingerCoreNETUnitTest/RunTestslib/CLITest.cs
@@ -996,8 +996,8 @@ namespace WorkspaceHold
             Assert.AreEqual(jiraUsrConfig.ALMServerURL, @"http:/Jira.CLI", "Validating correct ALMServerURL");
             Assert.AreEqual(jiraSolConfig.ALMUserName, "CLIUser", "Validating correct ALMUserName");
             Assert.AreEqual(jiraUsrConfig.ALMUserName, "CLIUser", "Validating correct ALMUserName");
-            Assert.AreEqual(jiraSolConfig.ALMPassword, "53d7ccd462fbfbc43c6dc9f3f638747582df333b", "Validating correct ALMPassword");
-            Assert.AreEqual(jiraUsrConfig.ALMPassword, "53d7ccd462fbfbc43c6dc9f3f638747582df333b", "Validating correct ALMPassword");
+            Assert.AreEqual(jiraSolConfig.ALMPassword, "JiraPassword", "Validating correct ALMPassword");
+            Assert.AreEqual(jiraUsrConfig.ALMPassword, "JiraPassword", "Validating correct ALMPassword");
             Assert.AreEqual(jiraSolConfig.JiraTestingALM, ALMIntegrationEnums.eTestingALMType.Xray, "Validating correct JiraTestingALM");
         }
 
