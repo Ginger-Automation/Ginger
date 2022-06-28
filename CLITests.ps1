7a7bd2e2a4c52cd05a51fa5e8e7650d3b306e20a
Mahesh Kale <MKALE@amdocs.com>
2022-06-29 01:40:58 +0530

    updated CLITests.ps file to get files from Net6.0 folder

--- CLITests.ps1
+++ CLITests.ps1
@@ -2,7 +2,7 @@ Write-Host "-------------------------------------------------------------"
 Write-Host "-                         Run CLI Tests                     -"
 Write-Host "-------------------------------------------------------------"
 
-cd /home/vsts/work/1/s/Ginger/GingerRuntime/bin/Release/net6.0/linux-x64/publish/
+cd /home/vsts/work/1/s/Ginger/GingerRuntime/bin/Release/net6.0/publish/
 
 
 Write-Host "******************************************************************************************************************************"
