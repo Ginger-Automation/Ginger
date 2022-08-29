e7747e03de732d2315696f09bfbefc8cab1046c3
Maheshkale447 <41791819+Maheshkale447@users.noreply.github.com>
2022-08-30 00:02:09 +0530

    Delete Ginger/Ginger.backup directory

--- Ginger/Ginger.backup/Imports/UFT/ObjectRepositortyItemProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Ginger.Imports.UFT
-{
-    public class ObjectRepositortyItemProperty
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
-    }
-}
