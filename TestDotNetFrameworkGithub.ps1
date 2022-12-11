bc7925fac5f5df4f2a5e6867696ff1ba5a18bffe
Nadeem Jazmawe <44744877+nadeemjazmawe@users.noreply.github.com>
2022-12-11 18:36:36 +0200

    Update TestDotNetFrameworkGithub.ps1

--- TestDotNetFrameworkGithub.ps1
+++ TestDotNetFrameworkGithub.ps1
@@ -10,13 +10,6 @@ mkdir D:\a\Ginger\Ginger\TestResults\DotNetFramework
 mkdir D:\a\1\a
 
 
-
-Write-Host "-------------------------------------------------------------"
-Write-Host "-                    print  path     1          -"
-Write-Host "-------------------------------------------------------------"
-dir D:\a\Ginger\Ginger\Ginger\GingerCoreTest\bin\Release
-
-
 ./vstest.console.exe "D:\a\Ginger\Ginger\Ginger\GingerCoreTest\bin\Release\net6.0-windows\GingerCoreTest.dll" /Logger:trx /ResultsDirectory:D:\a\Ginger\Ginger\TestResults\DotNetFramework
 Write-Host ">>>>>>>>>>>>>>>>>>>>>>>>>>>>> LastExitCode: " $LastExitCode
 if ($LastExitCode -ne 0)
@@ -28,7 +21,7 @@ if ($LastExitCode -ne 0)
 Write-Host "-------------------------------------------------------------"
 Write-Host "-                    print  path     1          -"
 Write-Host "-------------------------------------------------------------"
-dir D:\a\Ginger\Ginger\Ginger\GingerCoreTest\bin\Release\net6.0-windows
+dir D:\a\Ginger\Ginger\Ginger\bin\Release\
 
 ./vstest.console.exe "D:\a\Ginger\Ginger\Ginger\bin\Release\net6.0-windows\Ginger.dll" /Logger:trx /ResultsDirectory:D:\a\Ginger\Ginger\TestResults\DotNetFramework
 Write-Host ">>>>>>>>>>>>>>>>>>>>>>>>>>>>> LastExitCode: " $LastExitCode
@@ -49,22 +42,22 @@ Write-Host "-------------------------------------------------------------"
 New-Item -Path "D:\a\1\a" -Name "TestCompleted.txt" -ItemType "file" -Value "Test Completed Artifacts"
 
 
-Write-Host "-------------------------------------------------------------"
-Write-Host "-                    print  path     1          -"
-Write-Host "-------------------------------------------------------------"
-dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest
-Write-Host "-------------------------------------------------------------"
-Write-Host "-                    print  path     2          -"
-Write-Host "-------------------------------------------------------------"
-dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest\bin
-Write-Host "-------------------------------------------------------------"
-Write-Host "-                    print  path     3          -"
-Write-Host "-------------------------------------------------------------"
-dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest\bin\Release
-Write-Host "-------------------------------------------------------------"
-Write-Host "-                    print  path     4          -"
-Write-Host "-------------------------------------------------------------"
-dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest\bin\Release\net6.0\
+# Write-Host "-------------------------------------------------------------"
+# Write-Host "-                    print  path     1          -"
+# Write-Host "-------------------------------------------------------------"
+# dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest
+# Write-Host "-------------------------------------------------------------"
+# Write-Host "-                    print  path     2          -"
+# Write-Host "-------------------------------------------------------------"
+# dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest\bin
+# Write-Host "-------------------------------------------------------------"
+# Write-Host "-                    print  path     3          -"
+# Write-Host "-------------------------------------------------------------"
+# dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest\bin\Release
+# Write-Host "-------------------------------------------------------------"
+# Write-Host "-                    print  path     4          -"
+# Write-Host "-------------------------------------------------------------"
+# dir D:\a\Ginger\Ginger\Ginger\GingerUtilsTest\bin\Release\net6.0\
 
 
 
@@ -72,7 +65,7 @@ Compress-Archive -Path 'D:\a\Ginger\Ginger\Ginger\GingerUtilsTest\bin\Release\ne
 Compress-Archive -Path 'D:\a\Ginger\Ginger\Ginger\GingerCoreCommonTest\bin\Release\net6.0\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerCoreCommonTestArtifacts'
 Compress-Archive -Path 'D:\a\Ginger\Ginger\Ginger\GingerCoreNETUnitTest\bin\Release\net6.0\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerCoreNETUnitTestArtifacts'
 Compress-Archive -Path 'D:\a\Ginger\Ginger\Ginger\GingerConsoleTest\bin\Release\net6.0\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerConsoleTestArtifacts'
-Compress-Archive -Path 'D:\a\Ginger\Ginger\GingerAutoPilotTest\bin\Release\net6.0\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerAutoPilotTestArtifacts'
+Compress-Archive -Path 'D:\a\Ginger\Ginger\Ginger\GingerAutoPilotTest\bin\Release\net6.0\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerAutoPilotTestArtifacts'
 # Compress-Archive -Path 'd:\a\1\s\Ginger\GingerPluginCoreTest\bin\Release\netcoreapp3.1\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerPluginCoreTestArtifacts'
       
 # Compress-Archive -Path 'd:\a\1\s\Ginger\GingerCoreTest\bin\Release\netcoreapp3.1\TestArtifacts' -DestinationPath 'D:\a\1\a\GingerCoreTestTestArtifacts'
