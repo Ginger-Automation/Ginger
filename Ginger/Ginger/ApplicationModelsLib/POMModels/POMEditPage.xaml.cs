5222f9aeae1b6822e5eb7234ed19a9999f1173c9
Harsimranjeet Singh <ranjeetsingh8481@gmail.com>
2023-02-09 17:27:38 +0530

    Removed 'Suggested Activities' tab from 'Page Object Model' screen

--- Ginger/Ginger/ApplicationModelsLib/POMModels/POMEditPage.xaml.cs
+++ Ginger/Ginger/ApplicationModelsLib/POMModels/POMEditPage.xaml.cs
@@ -136,24 +136,6 @@ namespace Ginger.ApplicationModelsLib.POMModels
 
             mPomAllElementsPage = new PomAllElementsPage(mPOM, PomAllElementsPage.eAllElementsPageContext.POMEditPage);
             xUIElementsFrame.Content = mPomAllElementsPage;
-
-            if (WorkSpace.Instance.BetaFeatures.AutoGenerateActivities)
-            {
-                ObservableList<Activity> pomActivities = AutoGenerateFlows.CreatePOMActivitiesFromMetadata(mPOM);
-                //Shared Activities which uses current POM
-                ObservableList<Activity> sharedActivities = WorkSpace.Instance.SolutionRepository.GetAllRepositoryItems<Activity>();
-                IEnumerable<Activity> pomSharedActivities = sharedActivities.Where(x => x.Acts.Any(act => act is ActUIElement && ((ActUIElement)act).ElementLocateValue != null && ((ActUIElement)act).ElementLocateValue.Contains(mPOM.Guid.ToString())));
-                pomSharedActivities.ToList().ForEach(item => pomActivities.Add(item));
-
-                mActivitiesRepositoryViewPage = new ActivitiesRepositoryPage(pomActivities, new Context());
-                xSharedActivitiesFrame.Content = mActivitiesRepositoryViewPage;
-                xPomActivitiesTabItem.Visibility = Visibility.Visible;
-
-            }
-            else
-            {
-                xPomActivitiesTabItem.Visibility = Visibility.Collapsed;
-            }
             mPomAllElementsPage.raiseUIElementsCountUpdated += UIElementCountUpdatedHandler;
             UIElementTabTextBlockUpdate();
 
