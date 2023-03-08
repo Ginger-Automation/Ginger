4d3e00a87bea9765ddf91d1218d185d3f1aa86b7
Harsimranjeet Singh <ranjeetsingh8481@gmail.com>
2023-03-08 16:32:03 +0530

    resolving codacy issues

--- Ginger/GingerCoreCommon/Repository/BusinessFlowLib/Activity.cs
+++ Ginger/GingerCoreCommon/Repository/BusinessFlowLib/Activity.cs
@@ -126,7 +126,7 @@ namespace GingerCore
             //set fields default values
             mAutomationStatus = eActivityAutomationStatus.Development;
             mActionRunOption = eActionRunOption.StopActionsRunOnFailure;
-            Tags.CollectionChanged += (sender, e) => OnPropertyChanged(nameof(Tags));
+            Tags.CollectionChanged += (_, _) => OnPropertyChanged(nameof(Tags));
         }
 
         public override string ToString()
