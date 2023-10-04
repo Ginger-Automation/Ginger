621cdfcc07e6bbd43e543680a240e5d44be6be7c
Niharika Pradhan <pradhann@amdocs.com>
2023-10-04 17:29:29 +0530

    Changes made as per suggestion.

--- Ginger/Ginger/Activities/ActivitiesGroupSelectionPage.xaml.cs
+++ Ginger/Ginger/Activities/ActivitiesGroupSelectionPage.xaml.cs
@@ -66,7 +66,7 @@ namespace Ginger.BusinessFlowPages
             }
             else
             {
-                if (businessFlow.IsSharedRepositoryInstance.Equals(false))
+                if (!businessFlow.IsSharedRepositoryInstance)
                 {
                     xRadioPanel.Visibility = Visibility.Collapsed;
                     xNoteLable.Visibility = Visibility.Collapsed;
