ce9306a888a048b0df9f4768fa5b9f1853d0b8a3
Mahesh Kale <mahesh.kale@amdocs.com>
2023-12-29 14:19:51 +0530

    Fixed closed button on Solution details button

--- Ginger/Ginger/SolutionWindows/SolutionPage.xaml.cs
+++ Ginger/Ginger/SolutionWindows/SolutionPage.xaml.cs
@@ -175,7 +175,7 @@ namespace Ginger.SolutionWindows
             //replaceKeyBtn.Content = "Update Key";
             //replaceKeyBtn.Click += new RoutedEventHandler(ReplaceKeyBtn_Click);
             //winButtons.Add(replaceKeyBtn);
-            GingerCore.General.LoadGenericWindow(ref _pageGenericWin, App.MainWindow, windowStyle, "Solution Details", this, winButtons, true, "Cancel", CloseBtn_Click);
+            GingerCore.General.LoadGenericWindow(ref _pageGenericWin, App.MainWindow, windowStyle, "Solution Details", this, winButtons, true, "Cancel");
             return IsValidEncryptionKeyAdded;
         }
 
@@ -197,11 +197,6 @@ namespace Ginger.SolutionWindows
             ReplaceEncryptionKeyPage replaceEncryptionKeyPage = new ReplaceEncryptionKeyPage();
             _pageGenericWin.Close();
             IsValidEncryptionKeyAdded = replaceEncryptionKeyPage.ShowAsWindow(mSolution);
-        }
-
-        private void CloseBtn_Click(object sender, RoutedEventArgs e)
-        {
-            IsValidEncryptionKeyAdded = false;
         }
 
         private void SaveKeyBtn_Click(object sender, RoutedEventArgs e)
