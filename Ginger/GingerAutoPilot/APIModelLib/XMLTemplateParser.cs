22d18f96d527d0a61de9f225caddcdf9eb05f48f
AMAN PRASAD <44187990+AmanPrasad43@users.noreply.github.com>
2024-01-18 12:15:42 +0530

    Update XMLTemplateParser.cs

--- Ginger/GingerAutoPilot/APIModelLib/XMLTemplateParser.cs
+++ Ginger/GingerAutoPilot/APIModelLib/XMLTemplateParser.cs
@@ -69,7 +69,8 @@ namespace Amdocs.Ginger.Repository
             {
 
                 string UniqPlaceHolder = "{" + GetPlaceHolderName(XDN.LocalName.ToUpper()) + "}";
-                AMPList.Add(new AppModelParameter(UniqPlaceHolder, string.Empty, XDN.LocalName, XDN.XPath, new ObservableList<OptionalValue> { new OptionalValue() { Value = XDN.Value } }));
+                XDN.Value = UniqPlaceHolder;
+                AMPList.Add(new AppModelParameter(UniqPlaceHolder, string.Empty, XDN.LocalName, XDN.XPath, new ObservableList<OptionalValue> { new OptionalValue() { Value = XDN.Value, IsDefault=true } }));
 
                 if (XDN.Attributes != null && XDN.Attributes.Count > 0)
                 {
@@ -81,7 +82,8 @@ namespace Amdocs.Ginger.Repository
                         }
 
                         string UniqAttributePlaceHolder = "{" + GetPlaceHolderName(XmlAttribute.LocalName.ToUpper()) + "}";
-                        AMPList.Add(new AppModelParameter(UniqAttributePlaceHolder, string.Empty, XmlAttribute.LocalName, XDN.XPath, new ObservableList<OptionalValue> { new OptionalValue() { Value = XmlAttribute.Value} }));
+                        XmlAttribute.Value = UniqAttributePlaceHolder;
+                        AMPList.Add(new AppModelParameter(UniqAttributePlaceHolder, string.Empty, XmlAttribute.LocalName, XDN.XPath, new ObservableList<OptionalValue> { new OptionalValue() { Value = XmlAttribute.Value, IsDefault=true} }));
                     }
                 }
             }
