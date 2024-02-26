caae7f5226dac948caadac4d6983f2670cbb465c
prashelke <shelke11pravin@gmail.com>
2024-02-26 18:04:36 +0530

    Accessibility Functions Move to the Relative class

--- Ginger/GingerCoreNET/ActionsLib/UI/Web/ActAccessibilityTesting.cs
+++ Ginger/GingerCoreNET/ActionsLib/UI/Web/ActAccessibilityTesting.cs
@@ -3,8 +3,11 @@ using Amdocs.Ginger.Common.InterfacesLib;
 using Amdocs.Ginger.Common.Repository;
 using Amdocs.Ginger.Common.UIElement;
 using Amdocs.Ginger.Common.VariablesLib;
+using Amdocs.Ginger.CoreNET.Execution;
 using Amdocs.Ginger.CoreNET.Run;
 using Amdocs.Ginger.Repository;
+using Deque.AxeCore.Commons;
+using Deque.AxeCore.Selenium;
 using DocumentFormat.OpenXml.Drawing.Diagrams;
 using Ginger.Configurations;
 using GingerCore.Actions;
@@ -12,6 +15,7 @@ using GingerCore.Actions.Common;
 using GingerCore.Platforms;
 using GingerCoreNET.SolutionRepositoryLib.RepositoryObjectsLib.PlatformsLib;
 using Newtonsoft.Json;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -314,6 +318,128 @@ namespace Amdocs.Ginger.CoreNET.ActionsLib.UI.Web
             {
                 _SeverityItems = value;
 
+            }
+        }
+
+        public void AnalyserAccessibility(IWebDriver Driver,IWebElement element)
+        {
+            AxeBuilder axeBuilder = null;
+            try
+            {
+                axeBuilder = CreateAxeBuilder(Driver);
+                AxeResult axeResult = null;
+
+                if ((GetInputParamValue(ActAccessibilityTesting.Fields.Target) == ActAccessibilityTesting.eTarget.Element.ToString()))
+                {
+                    axeResult = axeBuilder.Analyze(element);
+                }
+                else
+                {
+                    axeResult = axeBuilder.Analyze();
+                }
+
+                SetAxeResultToAction(axeResult);
+            }
+            catch (Exception ex)
+            {
+                Error = "Error: during accessibility testing:" + ex.Message;
+                Reporter.ToLog(eLogLevel.ERROR, "Error: during accessibility testing", ex);
+                return;
+            }
+        }
+
+        public AxeBuilder CreateAxeBuilder(IWebDriver Driver)
+        {
+            AxeBuilder axeBuilder = null;
+            axeBuilder = new AxeBuilder(Driver)
+            .WithOptions(new AxeRunOptions()
+            {
+                XPath = true
+            });
+            List<string> sevritylist = SeverityOperationValueList.Select(x => x.Value.ToLower()).ToList();
+            if (GetInputParamValue(ActAccessibilityTesting.Fields.Analyzer) == ActAccessibilityTesting.eAnalyzer.ByStandard.ToString())
+            {
+                if (OperationValueList != null &&OperationValueList.Any())
+                {
+                    string[] Tag_array = OperationValueList.Select(i => i.Value.ToString()).ToArray();
+                    axeBuilder.WithTags(Tag_array);
+                }
+                string[] ExcludeRules = RulesItemsdata.Where(x => sevritylist.Contains(x.Impact.ToLower())).Select(i => i.RuleID.ToString()).ToArray();
+                if (ExcludeRules != null && ExcludeRules.Any())
+                {
+                    axeBuilder.DisableRules(ExcludeRules);
+                }
+            }
+            else if (GetInputParamValue(ActAccessibilityTesting.Fields.Analyzer) == ActAccessibilityTesting.eAnalyzer.BySeverity.ToString())
+            {
+                string[] IncludeRules = RulesItemsdata.Where(x => sevritylist.Contains(x.Impact.ToLower())).Select(i => i.RuleID.ToString()).ToArray();
+                if (IncludeRules != null && IncludeRules.Any())
+                {
+                    axeBuilder.WithRules(IncludeRules);
+                }
+            }
+            return axeBuilder;
+        }
+
+        public void SetAxeResultToAction(AxeResult axeResult)
+        {
+            bool hasAnyViolations = axeResult.Violations.Any();
+            bool ActionResult = false;
+
+            if (GetInputParamValue(ActAccessibilityTesting.Fields.Analyzer) == ActAccessibilityTesting.eAnalyzer.ByStandard.ToString() && SeverityOperationValueList != null && SeverityOperationValueList.Any())
+            {
+                List<string> sevritylist = SeverityOperationValueList.Select(x => x.Value.ToLower()).ToList();
+                List<string> Violationsevrity = axeResult.Violations.Any() ? axeResult.Violations.Select(x => x.Impact.ToLower()).ToList() : new List<string>();
+                foreach (string severity in sevritylist)
+                {
+                    ActionResult = !Violationsevrity.Any(y => y.Equals(severity));
+                }
+            }
+            else if (GetInputParamValue(ActAccessibilityTesting.Fields.Analyzer) == ActAccessibilityTesting.eAnalyzer.BySeverity.ToString() && SeverityOperationValueList != null && SeverityOperationValueList.Any())
+            {
+                List<string> sevritylist = SeverityOperationValueList.Select(x => x.Value.ToLower()).ToList();
+                List<string> Violationsevrity = axeResult.Violations.Any() ? axeResult.Violations.Select(x => x.Impact.ToLower()).ToList() : new List<string>();
+                foreach (string severity in sevritylist)
+                {
+                    ActionResult = Violationsevrity.Any(y => y.Equals(severity));
+                }
+            }
+            var jsonresponse = JsonConvert.SerializeObject(axeResult, Newtonsoft.Json.Formatting.Indented);
+            RawResponseValues = jsonresponse;
+            AddOrUpdateReturnParamActual(ParamName: "Raw Response", ActualValue: jsonresponse);
+            if (hasAnyViolations)
+            {
+                if (ActionResult)
+                {
+                    Status = eRunStatus.Failed;
+                }
+                else
+                {
+                    Status = eRunStatus.Passed;
+                }
+                Error = $"Accessibility testing resulted in violations.";
+                AddOrUpdateReturnParamActual(ParamName: "ViolationCount", ActualValue: axeResult.Violations.Length.ToString());
+                AddOrUpdateReturnParamActual(ParamName: "ViolationList", ActualValue: String.Join(",", axeResult.Violations.Select(x => x.Id)));
+                int violatedNodeIndex = 0;
+                foreach (AxeResultItem violation in axeResult.Violations)
+                {
+                    foreach (AxeResultNode node in violation.Nodes)
+                    {
+                        violatedNodeIndex++;
+                        AddOrUpdateReturnParamActualWithPath(ParamName: "ViolationId", ActualValue: violation.Id, violatedNodeIndex.ToString());
+                        if (node.XPath != null)
+                        {
+                            AddOrUpdateReturnParamActualWithPath(ParamName: "NodeXPath", ActualValue: node.XPath.ToString(), violatedNodeIndex.ToString());
+                        }
+
+                        AddOrUpdateReturnParamActualWithPath(ParamName: "ViolationHelp", ActualValue: violation.Help, violatedNodeIndex.ToString());
+                        AddOrUpdateReturnParamActualWithPath(ParamName: "ViolationSeverity", ActualValue: violation.Impact, violatedNodeIndex.ToString());
+                    }
+                }
+            }
+            else
+            {
+                Status = eRunStatus.Passed;
             }
         }
     }
