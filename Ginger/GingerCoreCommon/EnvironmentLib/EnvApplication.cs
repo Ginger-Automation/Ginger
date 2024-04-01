b118b158843442c43772f7534eaab6a77a5954b4
Manas Kalangan <manaska@amdocs.com>
2024-04-01 16:10:37 +0530

    backward compatibility for Action ValueExpressions

--- Ginger/GingerCoreCommon/EnvironmentLib/EnvApplication.cs
+++ Ginger/GingerCoreCommon/EnvironmentLib/EnvApplication.cs
@@ -83,6 +83,7 @@ namespace GingerCore.Environments
         }
         public VariableBase GetVariable(string ParamName)
         {
+            ConvertGeneralParamsToVariable();
             return Variables.FirstOrDefault((variable)=>variable.Name.Equals(ParamName));
 
         }
