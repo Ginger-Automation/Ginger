7663276abfbf02f0baa864a43c26c673236001c3
Harsimranjeet Singh <ranjeetsingh8481@gmail.com>
2024-05-23 16:40:11 +0530

    handle switch frame

--- Ginger/GingerCoreNET/Drivers/CoreDrivers/Web/IBrowserTab.cs
+++ Ginger/GingerCoreNET/Drivers/CoreDrivers/Web/IBrowserTab.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amdocs.Ginger.Common.UIElement;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +34,8 @@ namespace Amdocs.Ginger.CoreNET.Drivers.CoreDrivers.Web
         public Task<string> GetConsoleLogsAsync();
 
         public Task<string> GetBrowserLogsAsync();
+
+        public Task<bool> SwitchFrame(eLocateBy locatyBy, string value);
 
         public Task CloseAsync();
     }
