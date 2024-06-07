50ee7e6f8354dd41e5c6a7f35b6b53eb722c8225
Sudarshan Sathe <sudarshan.sathe@amdocs.com>
2024-06-07 14:46:47 +0530

    using string interpolation

--- Ginger/Ginger/Actions/UserControls/UCArtifact.xaml.cs
+++ Ginger/Ginger/Actions/UserControls/UCArtifact.xaml.cs
@@ -61,8 +61,8 @@ namespace Ginger.Actions.UserControls
             {
                 string FileName = string.Empty;
                 if(!string.IsNullOrEmpty(ArtifactPath))
-                {
-                    FileName = ArtifactName + " (" + System.IO.Path.GetFileName(ArtifactPath) +")";
+                {                    
+                   FileName = $"{ArtifactName} ({System.IO.Path.GetFileName(ArtifactPath)})";
                 }
                 return FileName;
             }            
