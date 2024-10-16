915e2e2659b5cd29b41ad53fe28c055d90519e64
khari thomas <tkhari77@gmail.com>
2024-10-16 11:22:39 -0500

    fix re-initialization bug

--- Ginger/Ginger/Actions/ActionEditPages/ActUIElementLib/UIElementScrollToOptionsPage.xaml.cs
+++ Ginger/Ginger/Actions/ActionEditPages/ActUIElementLib/UIElementScrollToOptionsPage.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using GingerCore.Actions.Common;
-using GingerCore.GeneralLib;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Controls;
 
 namespace Ginger.Actions._Common.ActUIElementLib
@@ -12,9 +9,7 @@ namespace Ginger.Actions._Common.ActUIElementLib
     /// </summary>
     public partial class UIElementScrollToOptionsPage : Page
     {
-        private readonly ActUIElement _action;
-
-        private enum eScrollAlignment
+        public enum eScrollAlignment
         {
             Start,
             Center,
@@ -26,21 +21,14 @@ namespace Ginger.Actions._Common.ActUIElementLib
         {
             InitializeComponent();
 
-            _action = action;
+            List<eScrollAlignment> scrollPositions = [
+                eScrollAlignment.Start,
+                eScrollAlignment.Center,
+                eScrollAlignment.End,
+                eScrollAlignment.Nearest,
+            ];
 
-            // Generate list of options from Enum
-            List<ComboItem> _scrollPositions = Enum.GetValues(typeof(eScrollAlignment))
-                .Cast<eScrollAlignment>()
-                .Select(pos => new ComboItem()
-                {
-                    text = pos.ToString(),
-                    Value = (int)pos
-                })
-                .Cast<ComboItem>()
-                .ToList();
-
-            // Bind alignment options with combo box
-            scrollAlignmentComboBox.Init(_action.GetOrCreateInputParam(ActUIElement.Fields.ScrollAlignment, eScrollAlignment.Start.ToString()), _scrollPositions, false);
+            scrollAlignmentComboBox.Init(action.GetOrCreateInputParam(ActUIElement.Fields.ScrollAlignment), scrollPositions, false);
         }
     }
 }
