2c1973db71889a1d7c028f5cb09695bdf4bf1373
Jainam Mehta <jainamm@amdocs.com>
2025-07-31 16:20:39 +0530

    size updated in test error

--- Ginger/GingerCoreNETUnitTest/SolutionTestsLib/RepositorySerializerTest.cs
+++ Ginger/GingerCoreNETUnitTest/SolutionTestsLib/RepositorySerializerTest.cs
@@ -97,7 +97,7 @@ namespace GingerCoreNETUnitTests.SolutionTestsLib
             // Please double verify if the increase in size make sense and is needed before changing this value of expected length            
             int lt = xml.Count(f => f == '<');
             int gt = xml.Count(f => f == '>');
-            Assert.IsTrue(xml.Length < 3000, "Verify minimal xml is less than 900 bytes");
+            Assert.IsTrue(xml.Length < 3000, "Verify minimal xml is less than 3000 bytes");
             Assert.AreEqual(32, lt, "XML Elements count <");
             Assert.AreEqual(32, gt, "XML Elements count >");
 
