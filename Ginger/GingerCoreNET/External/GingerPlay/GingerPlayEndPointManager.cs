d8e661ac9e5f6914cf49eb03d9f937e43c9f0272
Mahesh Kale <MKALE@amdocs.com>
2025-08-04 22:59:35 +0530

    Updated Ginger Play Itegtration to remove Ginger Html Report Service URL

--- Ginger/GingerCoreNET/External/GingerPlay/GingerPlayEndPointManager.cs
+++ Ginger/GingerCoreNET/External/GingerPlay/GingerPlayEndPointManager.cs
@@ -28,7 +28,7 @@ namespace Amdocs.Ginger.CoreNET.External.GingerPlay
     {
         private static readonly string ACCOUNT_REPORT_SERVICE_URL = System.Configuration.ConfigurationManager.AppSettings["ACCOUNT_REPORT_SERVICE_URL"]?.ToString() ?? "ginger-report";
 
-        private static readonly string HTML_REPORT_SERVICE_URL = System.Configuration.ConfigurationManager.AppSettings["HTML_REPORT_SERVICE_URL"]?.ToString() ?? "ginger-html-report";
+        //private static readonly string HTML_REPORT_SERVICE_URL = System.Configuration.ConfigurationManager.AppSettings["HTML_REPORT_SERVICE_URL"]?.ToString() ?? "ginger-html-report";
 
         private static readonly string AI_SERVICE_URL = System.Configuration.ConfigurationManager.AppSettings["AI_SERVICE_URL"]?.ToString() ?? "ginger-ai";
 
@@ -84,14 +84,14 @@ namespace Amdocs.Ginger.CoreNET.External.GingerPlay
                 else
                 {
                     Reporter.ToLog(eLogLevel.ERROR, "Error occurred while getting Account Report Service URL");
-                    
+
                     return null;
                 }
             }
             catch (Exception ex)
             {
                 Reporter.ToLog(eLogLevel.ERROR, $"Error occurred while getting Account Report Service URL: {ex.Message}");
-                
+
                 return null;
             }
 
@@ -103,7 +103,7 @@ namespace Amdocs.Ginger.CoreNET.External.GingerPlay
             {
                 if (!string.IsNullOrEmpty(GingerPlayConfiguration.GingerPlayGatewayUrl) && GingerPlayConfiguration.GingerPlayReportServiceEnabled)
                 {
-                    return GingerPlayConfiguration.GingerPlayGatewayUrl + HTML_REPORT_SERVICE_URL;
+                    return GingerPlayConfiguration.GingerPlayGatewayUrl + ACCOUNT_REPORT_SERVICE_URL;
                 }
                 else if (!string.IsNullOrEmpty(GingerPlayConfiguration.CentralizedHTMLReportServiceURL))
                 {
